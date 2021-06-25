01a76fd8868b8798b157de052d1d6b1695194adf
ArtisticSoftware <artisticsoftware2@gmail.com>
2021-06-25 04:05:57 +0000

    Restricting the criteria to parity

--- sortodds.php
+++ sortodds.php
@@ -5,10 +5,6 @@ if(!isset($argv[1])) die("Race Date Not Entered!!\n");
 $raceDate = trim($argv[1]);
 $currentDir = __DIR__ . DIRECTORY_SEPARATOR . $raceDate;
 
-function isPrime($x){
-    return in_array($x, [2, 3, 5, 7, 11, 13, 17, 19, 23]);
-}
-
 $allOdds = include($currentDir . DIRECTORY_SEPARATOR . "getodds.php");
 $probas = [];
 
@@ -44,19 +40,17 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
     $outtext .= "\t\t*/\n";
     $tmpArray = $probas[$raceNumber];
     $tmpSum = 0;
-    $oddN = 0;
-    $evenN = 0;
-    $primeN = 0;
+    $indicators = ['Odds' => 0, 'Evens' => 0];
     foreach ($tmpArray as $i => $val){
         $outtext .= "\t\t$i => $val,\n";
-        if($i % 2 === 0) $evenN += $val;
-        else $oddN += $val;
-        if(isPrime($i)) $primeN += $val;
+        if($i % 2 === 0) $indicators['Evens'] += $val;
+        else $indicators['Odds'] += $val;
         $tmpSum += $val;
     }
-    $outtext .= "\t\t'Primes' => $primeN,\n";
-    $outtext .= "\t\t'Odds' => $oddN,\n";
-    $outtext .= "\t\t'Evens' => $evenN,\n";
+    arsort($indicators);
+    foreach($indicators as $label => $indicator) {
+        $outtext .= "\t\t'$label' => $indicator,\n";
+    }
     $outtext .= "\t\t'Sum' => $tmpSum,\n";
     $outtext .= "\t],\n";
 }
