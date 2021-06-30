0fe2fac661f2e34448868f0cc836d8354bd24172
ArtisticSoftware <artisticsoftware2@gmail.com>
2021-06-30 05:51:42 +0000

    0701

--- getodds.php
+++ getodds.php
@@ -1,9 +1,20 @@
 <?php
 
-if(isset($argv[1])) $totalRaces = trim($argv[1]);
+if(!isset($argv[1])) die("Race Date Not Entered!!\n");
+
+$raceDate = trim($argv[1]);
+$outDir = __DIR__ . DIRECTORY_SEPARATOR . $raceDate;
+$outFile =$outDir . DIRECTORY_SEPARATOR . "getodds.php";
+
+$outtext = "<?php\n\n";
+$outtext .= "return [\n";
+
+if(isset($argv[2])) $totalRaces = trim($argv[2]);
 else $totalRaces = 10;
 
 for($r = 1; $r <= $totalRaces; $r++){
+
+    $outtext .= "\t$r => [\n";
 
     $oddsJSON = file_get_contents("https://bet.hkjc.com/racing/getJSON.aspx?type=winplaodds&date=2021-07-01&venue=ST&start=$r&end=$r");
 
@@ -16,8 +27,18 @@ for($r = 1; $r <= $totalRaces; $r++){
     $odds = explode(";",$odds);
 
     for($k = 1; $k < count($odds); $k++) {
-        var_dump($odds[$k]);
+        $lineParts = explode("=", $odds[$k]);
+        $runner = $lineParts[0];
+        $currentOdds = $lineParts[1];
+        if($currentOdds !== "SCR"){
+            $outtext .= "\t\t$runner => $currentOdds,\n";
+        }
     }
-    die();
-
+    $outtext .= "\t],\n";
 }
+
+$outtext .= "];\n";
+
+file_put_contents($outFile, $outtext);
+
+
