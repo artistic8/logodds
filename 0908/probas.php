5ba50824fe7d9e989cb269d463ac68395d4ab3ea
artistic3 <artisticsoftware3@gmail.com>
2021-09-07 02:24:46 +0000

    ...

---
+++ 0908/probas.php
@@ -0,0 +1,76 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'Equal positions R(O) and B(E)' => true,
+		'B' => 0,
+		'R' => 0,
+		'Sum' => 0,
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'Equal positions R(O) and B(E)' => true,
+		'B' => 0,
+		'R' => 0,
+		'Sum' => 0,
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'Equal positions R(O) and B(E)' => true,
+		'B' => 0,
+		'R' => 0,
+		'Sum' => 0,
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'Equal positions R(O) and B(E)' => true,
+		'B' => 0,
+		'R' => 0,
+		'Sum' => 0,
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'Equal positions R(O) and B(E)' => true,
+		'B' => 0,
+		'R' => 0,
+		'Sum' => 0,
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'Equal positions R(O) and B(E)' => true,
+		'B' => 0,
+		'R' => 0,
+		'Sum' => 0,
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'Equal positions R(O) and B(E)' => true,
+		'B' => 0,
+		'R' => 0,
+		'Sum' => 0,
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'Equal positions R(O) and B(E)' => true,
+		'B' => 0,
+		'R' => 0,
+		'Sum' => 0,
+	],
+];
