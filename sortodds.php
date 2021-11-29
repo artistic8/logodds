fcb73afdfd72aa1dfb523dcc6a787cb5032a2154
artistic3 <87168661+artistic3@users.noreply.github.com>
2021-11-29 20:45:58 +0800

    Update sortodds.php

--- sortodds.php
+++ sortodds.php
@@ -108,7 +108,7 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
 
     $outtext .= "\t\t'Poor man\'s bet' ,\n";
     $outtext .= "\t\t\t//'Win' => '" . implode(", ", $poorWin) . "'" . ",\n";
-    $outtext .= "\t\t\t'QQpl($30)' => '$first1, $second',\n";
+    $outtext .= "\t\t\t'QQpl($20)' => '$first1, $second',\n";
     $outtext .= "\t\t'QQpl($10)' ,\n" . "\t\t\t'" . $qpl10 . "'" . ",\n";
     $outtext .= "\t\t'Qin($10)' ,\n" . "\t\t\t'" . $qpl30 . "'" . ",\n";
     // $outtext .= "\t\t'Qpl($30)' ,\n" . "\t\t\t'" . $qpl30 . "'" . ",\n";
