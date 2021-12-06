85ed256096643d51a57c38f3fc7aa3cd46e46798
artistic41 <artisticsoftware41@gmail.com>
2021-12-06 08:13:28 +0000

    ...

--- sortodds.php
+++ sortodds.php
@@ -54,10 +54,11 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
 
     $values = array_values($tmpArray);
 
-    $outtext .= "\t\t'" . implode(", ", $runners) . "',\n";
     $first4 = array_slice($runners, 0, 4);
     $sBlacks = array_values(array_intersect($runners, $blacks));
+    $outtext .= "\t\tB: '" . implode(", ", $sBlacks) . "',\n";
     $sReds = array_values(array_intersect($runners, $reds));
+    $outtext .= "\t\tR: '" . implode(", ", $sReds) . "',\n";
    
     $first1 = $runners[0];
     $missing = array_diff($missing,[$first1]);
