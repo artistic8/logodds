0634ad507a1b8e31add1a591a435b94b592b3fad
artistic3 <87168661+artistic3@users.noreply.github.com>
2022-11-27 20:06:37 +0800

    Update sortodds.php

--- sortodds.php
+++ sortodds.php
@@ -86,10 +86,15 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
     sort($qin);
     sort($tce);
 
-    $racetext .= "\t\t'Tce' =>  '" . implode(", ", $tce) . "',\n";          
-    $racetext .= "\t\t'Qin' =>  '" . implode(", ", $qin) . "',\n";          
-    $racetext .= "\t\t'qin/qpl/fct' =>  '" . $favorites[0] . ", " . $favorites[1] . "',\n";          
-    $racetext .= "\t\t'qin/qpl/fct' =>  '" . $favorites[0] . ", " . $others[0] . "',\n";          
+    $racetext .= "\t\t'qpl' =>  '" . $favorites[0] . " X " . $others[0] . ", " . $favorites[2]. ", " . $favorites[4] . "',\n";    
+    $racetext .= "\t\t'qpl' =>  '" . $favorites[0] . " X " . $others[2]. ", " . $others[4] . "',\n";          
+    $racetext .= "\t\t'qpl' =>  '" . $others[0] . " X " . $favorites[2]. ", " . $favorites[4] . "',\n";          
+    $racetext .= "\t\t'qpl' =>  '" . $others[0] . " X " . $others[2]. ", " . $others[4] . "',\n";          
+    
+    $racetext .= "\t\t'qpl' =>  '" . $favorites[1] . " X " . $others[1] . ", " . $favorites[3]. ", " . $favorites[5] . "',\n";    
+    $racetext .= "\t\t'qpl' =>  '" . $favorites[1] . " X " . $others[3]. ", " . $others[5] . "',\n";    
+    $racetext .= "\t\t'qpl' =>  '" . $others[1] . " X " . $favorites[3]. ", " . $favorites[5] . "',\n";    
+    $racetext .= "\t\t'qpl' =>  '" . $others[1] . " X " . $others[3]. ", " . $others[5] . "',\n";    
 
     $racetext .= "\t],\n";
 
