971a2ad1ead82edfdd0938437542d03f820d1efe
artistic3 <artisticsoftware3@gmail.com>
2022-11-30 14:42:29 +0000

    ...

--- 1130/probas.php
+++ 1130/probas.php
@@ -5,67 +5,48 @@ return [
 		/**
 		Race 1
 		*/
-		'Subset0' =>  '2, 1',
-		'Subset1' =>  '11, 7',
-		'Subset3' =>  '4, 12',
+		'Place' =>  '2',
 	],
 	'2' => [
 		/**
 		Race 2
 		*/
-		'Subset0' =>  '6, 7',
-		'Subset1' =>  '8, 3',
-		'Subset2' =>  '11, 1',
-		'Subset3' =>  '2, 9',
+		'Place' =>  '6',
 	],
 	'3' => [
 		/**
 		Race 3
 		*/
-		'Subset0' =>  '8, 7',
-		'Subset1' =>  '1, 2',
-		'Subset2' =>  '4, 11',
-		'Subset3' =>  '5, 3',
+		'Place' =>  '8',
 	],
 	'4' => [
 		/**
 		Race 4
 		*/
-		'Subset0' =>  '1, 5',
-		'Subset1' =>  '4, 2',
-		'Subset3' =>  '7, 8',
+		'Place' =>  '1',
 	],
 	'5' => [
 		/**
 		Race 5
 		*/
-		'Subset0' =>  '1, 2',
-		'Subset1' =>  '5, 6',
-		'Subset2' =>  '7, 8',
-		'Subset3' =>  '9, 10',
+		'Place' =>  '9',
 	],
 	'6' => [
 		/**
 		Race 6
 		*/
-		'Subset0' =>  '3, 4',
-		'Subset1' =>  '2, 12',
-		'Subset3' =>  '1, 9',
+		'Place' =>  '3',
 	],
 	'7' => [
 		/**
 		Race 7
 		*/
-		'Subset0' =>  '3, 12',
-		'Subset1' =>  '10, 1',
-		'Subset3' =>  '9, 4',
+		'Place' =>  '3',
 	],
 	'8' => [
 		/**
 		Race 8
 		*/
-		'Subset1' =>  '2, 10',
-		'Subset2' =>  '11, 8',
-		'Subset3' =>  '1, 12',
+		'Place' =>  '8',
 	],
 ];
