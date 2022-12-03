82338fd8d90116a33f700a3213da71de34c43b5d
artistic3 <artisticsoftware3@gmail.com>
2022-12-03 23:02:13 +0000

    ...

--- 1204/place.php
+++ 1204/place.php
@@ -5,17 +5,17 @@ return [
 		/**
 		Race 1
 		*/
-		'Place 1' =>  '5',
+		'Place 1' =>  '1',
 		'Place 2' =>  '3',
-		'Place 3' =>  '1',
+		'Place 3' =>  '12',
 	],
 	'2' => [
 		/**
 		Race 2
 		*/
-		'Place 1' =>  '2',
-		'Place 2' =>  '8',
-		'Place 3' =>  '7',
+		'Place 1' =>  '8',
+		'Place 2' =>  '2',
+		'Place 3' =>  '5',
 	],
 	'3' => [
 		/**
@@ -29,56 +29,56 @@ return [
 		/**
 		Race 4
 		*/
-		'Place 1' =>  '4',
-		'Place 2' =>  '6',
-		'Place 3' =>  '8',
+		'Place 1' =>  '',
+		'Place 2' =>  '',
+		'Place 3' =>  '',
 	],
 	'5' => [
 		/**
 		Race 5
 		*/
-		'Place 1' =>  '1',
-		'Place 2' =>  '12',
-		'Place 3' =>  '5',
+		'Place 1' =>  '9',
+		'Place 2' =>  '11',
+		'Place 3' =>  '10',
 	],
 	'6' => [
 		/**
 		Race 6
 		*/
 		'Place 1' =>  '5',
-		'Place 2' =>  '2',
-		'Place 3' =>  '10',
+		'Place 2' =>  '7',
+		'Place 3' =>  '3',
 	],
 	'7' => [
 		/**
 		Race 7
 		*/
-		'Place 1' =>  '6',
-		'Place 2' =>  '3',
-		'Place 3' =>  '5',
+		'Place 1' =>  '5',
+		'Place 2' =>  '14',
+		'Place 3' =>  '1',
 	],
 	'8' => [
 		/**
 		Race 8
 		*/
-		'Place 1' =>  '2',
-		'Place 2' =>  '1',
-		'Place 3' =>  '13',
+		'Place 1' =>  '3',
+		'Place 2' =>  '7',
+		'Place 3' =>  '5',
 	],
 	'9' => [
 		/**
 		Race 9
 		*/
 		'Place 1' =>  '2',
-		'Place 2' =>  '3',
-		'Place 3' =>  '4',
+		'Place 2' =>  '11',
+		'Place 3' =>  '3',
 	],
 	'10' => [
 		/**
 		Race 10
 		*/
-		'Place 1' =>  '2',
-		'Place 2' =>  '12',
-		'Place 3' =>  '1',
+		'Place 1' =>  '4',
+		'Place 2' =>  '2',
+		'Place 3' =>  '12',
 	],
 ];
