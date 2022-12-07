e4fcb0c59d8a75f59c2e6bea7eafffaf88870695
artistic3 <artisticsoftware3@gmail.com>
2022-12-07 00:04:17 +0000

    ...

---
+++ 1207/1207.php
@@ -0,0 +1,79 @@
+<?php
+
+/**	 Most Common Occurence 	*/
+/**		Number 3 shows 5 Times	*/
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'Win' =>  '3',
+		'Place 1' =>  '7',
+		'Place 2' =>  '3',
+		'Place 3' =>  '5',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'Win' =>  '3',
+		'Place 1' =>  '3',
+		'Place 2' =>  '7',
+		'Place 3' =>  '10',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'Win' =>  '4',
+		'Place 1' =>  '4',
+		'Place 2' =>  '2',
+		'Place 3' =>  '11',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'Win' =>  '1',
+		'Place 1' =>  '7',
+		'Place 2' =>  '10',
+		'Place 3' =>  '1',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'Win' =>  '3',
+		'Place 1' =>  '1',
+		'Place 2' =>  '3',
+		'Place 3' =>  '4',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'Win' =>  '12',
+		'Place 1' =>  '9',
+		'Place 2' =>  '5',
+		'Place 3' =>  '2',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'Win' =>  '3',
+		'Place 1' =>  '3',
+		'Place 2' =>  '1',
+		'Place 3' =>  '12',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'Win' =>  '3',
+		'Place 1' =>  '3',
+		'Place 2' =>  '7',
+		'Place 3' =>  '1',
+	],
+];
