0aadc38eeea91727c5f5e514c40bed437a72b6e7
artistic3 <artisticsoftware3@gmail.com>
2022-12-11 09:07:13 +0000

    ...

--- 1211/1211.php
+++ 1211/1211.php
@@ -1,13 +1,13 @@
 <?php
 
 /**	Most Common Occurence */	
-/**	Number 9 shows 1 Times */
+/**	Number 4 shows 2 Times */
 /**	To bet PLACE in races where */
 /**	it's suggested as WIN or PLACE */
 /**	And Place payout > 1.5 */
 
 /**	Second Most Common Occurence */
-/**	Number 8 shows 1 Times */
+/**	Number 6 shows 2 Times */
 /**	To bet WIN in races where */
 /**	Most Common Occurence is suggested as WIN */
 
@@ -46,16 +46,16 @@ return [
 		'Win' =>  '1',
 		'Place 1' =>  '4',
 		'Place 2' =>  '1',
-		'Place 3' =>  '14',
+		'Place 3' =>  '13',
 	],
 	'6' => [
 		/**
 		Race 6
 		*/
-		'Win' =>  '3',
+		'Win' =>  '4',
 		'Place 1' =>  '3',
 		'Place 2' =>  '4',
-		'Place 3' =>  '1',
+		'Place 3' =>  '6',
 	],
 	'8' => [
 		/**
@@ -63,14 +63,14 @@ return [
 		*/
 		'Win' =>  '6',
 		'Place 1' =>  '6',
-		'Place 2' =>  '3',
-		'Place 3' =>  '12',
+		'Place 2' =>  '4',
+		'Place 3' =>  '8',
 	],
 	'9' => [
 		/**
 		Race 9
 		*/
-		'Win' =>  '10',
+		'Win' =>  '6',
 		'Place 1' =>  '6',
 		'Place 2' =>  '10',
 		'Place 3' =>  '12',
@@ -80,8 +80,8 @@ return [
 		Race 10
 		*/
 		'Win' =>  '4',
-		'Place 1' =>  '4',
-		'Place 2' =>  '8',
+		'Place 1' =>  '8',
+		'Place 2' =>  '4',
 		'Place 3' =>  '10',
 	],
 ];
