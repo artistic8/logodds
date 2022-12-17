12ac1b53d37ed1c22d2fe66eca2abec39a4aec32
artistic3 <artisticsoftware3@gmail.com>
2022-12-17 04:37:28 +0000

    ...

---
+++ 1218/1218.php
@@ -0,0 +1,104 @@
+<?php
+
+/**	Most Common Occurence */	
+/**	Number 2 shows 3 Times */
+/**	To bet QPL in races where */
+/**	it's suggested as WIN or PLACE */
+
+/**	Second Most Common Occurence */
+/**	Number 8 shows 2 Times */
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'Win' =>  '6',
+		'Place 1' =>  '2',
+		'Place 2' =>  '7',
+		'Place 3' =>  '14',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'Win' =>  '2',
+		'Place 1' =>  '9',
+		'Place 2' =>  '5',
+		'Place 3' =>  '11',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'Win' =>  '2',
+		'Place 1' =>  '2',
+		'Place 2' =>  '3',
+		'Place 3' =>  '13',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'Win' =>  '5',
+		'Place 1' =>  '5',
+		'Place 2' =>  '3',
+		'Place 3' =>  '10',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'Win' =>  '2',
+		'Place 1' =>  '2',
+		'Place 2' =>  '6',
+		'Place 3' =>  '13',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'Win' =>  '4',
+		'Place 1' =>  '9',
+		'Place 2' =>  '4',
+		'Place 3' =>  '3',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'Win' =>  '1',
+		'Place 1' =>  '1',
+		'Place 2' =>  '6',
+		'Place 3' =>  '5',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'Win' =>  '8',
+		'Place 1' =>  '8',
+		'Place 2' =>  '3',
+		'Place 3' =>  '1',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'Win' =>  '3',
+		'Place 1' =>  '3',
+		'Place 2' =>  '12',
+		'Place 3' =>  '1',
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'Win' =>  '8',
+		'Place 1' =>  '4',
+		'Place 2' =>  '13',
+		'Place 3' =>  '2',
+	],
+];
+
+?>
