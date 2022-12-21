004f6de602f67740928d24aa14762a6ecd34f3dd
artistic3 <artisticsoftware3@gmail.com>
2022-12-21 12:52:56 +0000

    ...

--- 1221/1221.php
+++ 1221/1221.php
@@ -1,11 +1,19 @@
 <?php
 
 /**	Most Common Occurence */	
-/**	Number 0 shows 0 Times */
+/**	Number 4 shows 2 Times */
 /**	Second Most Common Occurence */	
-/**	Number 0 shows 0 Times */
+/**	Number 1 shows 1 Times */
 
 return [
+	'Race 4' => 
+	[
+		'QPL' => [4, 3, 8]
+	],
+	'Race 5' => 
+	[
+		'Win, Qin' => [4, 3, 1, 7]
+	],
 ];
 
 ?>
