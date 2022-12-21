b1a3f787b0ba4d241be04e4c3a0e902a0c6dcf10
artistic3 <artisticsoftware3@gmail.com>
2022-12-21 09:56:13 +0000

    ...

--- 1221/1221.php
+++ 1221/1221.php
@@ -6,13 +6,13 @@
 /**	Number 1 shows 1 Times */
 
 return [
+	'Race 5' => 
+	[
+		'QPL' => [3, 1, 7]
+	],
 	'Race 6' => 
 	[
 		'QPL' => [3, 7, 4]
-	],
-	'Race 8' => 
-	[
-		'Win, Qin' => [3, 2, 11, 4]
 	],
 ];
 
