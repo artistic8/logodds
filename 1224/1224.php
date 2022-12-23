eee78a898813794c4e2e256ccfb06e48586de6aa
artistic3 <artisticsoftware3@gmail.com>
2022-12-23 05:47:09 +0000

    ...

---
+++ 1224/1224.php
@@ -0,0 +1,19 @@
+<?php
+
+/**	Most Common Occurence */	
+/**	Number 4 shows 2 Times */
+/**	Second Most Common Occurence */	
+/**	Number 11 shows 1 Times */
+
+return [
+	'Race 4' => 
+	[
+		'QPL' => [4, 12, 1]
+	],
+	'Race 9' => 
+	[
+		'QPL' => [4, 9, 2]
+	],
+];
+
+?>
