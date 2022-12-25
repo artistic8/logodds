ae5052a91ad19e0c67f487980b01dac690150d44
artistic3 <artisticsoftware3@gmail.com>
2022-12-25 02:35:31 +0000

    ...

--- 1225/1225.php
+++ 1225/1225.php
@@ -1,27 +1,11 @@
 <?php
 
 /**	Most Common Occurence */	
-/**	Number 2 shows 2 Times */
+/**	Number 0 shows 0 Times */
 /**	Second Most Common Occurence */	
-/**	Number 9 shows 2 Times */
+/**	Number 0 shows 0 Times */
 
 return [
-	'Race 1' => 
-	[
-		'QPL' => [2, 10, 4]
-	],
-	'Race 5' => 
-	[
-		'Win, Qin' => [9, 5, 16, 3]
-	],
-	'Race 6' => 
-	[
-		'QPL' => [9, 13, 5]
-	],
-	'Race 7' => 
-	[
-		'QPL' => [2, 10, 12]
-	],
 ];
 
 ?>
