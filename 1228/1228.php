ce953e39b21838471b1e9a8e75e0d167e85606e0
artistic3 <artisticsoftware3@gmail.com>
2022-12-27 23:21:05 +0000

    ...

--- 1228/1228.php
+++ 1228/1228.php
@@ -1,26 +1,38 @@
 <?php
 
 /**	Most Common Occurence */	
-/**	Number 3 shows 3 Times */
-/**	Second Most Common Occurence */	
 /**	Number 2 shows 2 Times */
+/**	Second Most Common Occurence */	
+/**	Number 3 shows 2 Times */
 
 return [
+	'Race 1' => 
+	[
+		'QPL' => [2, 5, 10]
+	],
 	'Race 3' => 
 	[
-		'QPL' => [3, 1, 12]
+		'QPL' => [10, 3, 7]
+	],
+	'Race 4' => 
+	[
+		'QPL' => [1, 12, 3]
 	],
 	'Race 5' => 
 	[
-		'QPL' => [3, 5, 8]
+		'QPL' => [3, 5, 12]
 	],
 	'Race 6' => 
 	[
-		'QPL' => [8, 3, 1]
+		'QPL' => [8, 3, 12]
 	],
 	'Race 7' => 
 	[
-		'QPL' => [3, 1, 6]
+		'QPL' => [3, 12, 7]
+	],
+	'Race 9' => 
+	[
+		'Win, Qin' => [2, 6, 8, 1]
 	],
 ];
 
