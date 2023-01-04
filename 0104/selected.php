7eb935e16c2d7a00e56ee2915ce14d4f15b2ef11
artistic3 <artisticsoftware3@gmail.com>
2023-01-04 12:29:16 +0000

    ...

--- 0104/selected.php
+++ 0104/selected.php
@@ -3,13 +3,13 @@
 /**	Most Common Occurence */	
 /**	Number 3 shows 3 Times */
 /**	Second Most Common Occurence */
-/**	Number 2 shows 1 Times */
+/**	Number 7 shows 2 Times */
 
 return [
 	'1' => [ 3, 2, 3, 5],
 	'2' => [ 2, 2, 10, 11],
 	'3' => [ 7, 7, 2, 10],
-	'4' => [ 1, 1, 12, 3],
+	'4' => [ 7, 7, 10, 11],
 	'5' => [ 5, 7, 5, 12],
 	'6' => [ 9, 9, 6, 2],
 	'7' => [ 4, 4, 3, 1],
