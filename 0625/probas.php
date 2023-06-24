b31f79292a959e258a5d5193f4ed70bb5287bdae
Artistic Software <artisticsoftware7@gmail.com>
2023-06-24 05:39:15 +0000

    ...

---
+++ 0625/probas.php
@@ -0,0 +1,107 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'Win' =>  '3',
+		'wins' =>  [[3]] ,
+		'inters' =>  [[2, 10, 13, 14]] ,
+		'dInter' =>  '',
+		'dInter-diff' =>  '',
+		'iInter' =>  '',
+		'iInter-diff' =>  '',
+		'qin' =>  '2, 10, 13, 14 X 3',
+		'qpl/trio' =>  [[3, 14, 2, 10, 13]] ,
+		'diff' =>  '',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'wins' =>  [] ,
+		'inters' =>  [[3, 5, 6, 8, 10]] ,
+		'dInter' =>  '',
+		'dInter-diff' =>  '',
+		'iInter' =>  '',
+		'iInter-diff' =>  '',
+		'qin' =>  '3, 5, 6, 8, 10',
+		'qpl/trio' =>  [[3, 5, 6, 8, 10]] ,
+		'diff' =>  '',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'Win' =>  '5, 6, 14',
+		'wins' =>  [[5, 6, 14]] ,
+		'inters' =>  [[2, 7]] ,
+		'dInter' =>  '',
+		'dInter-diff' =>  '',
+		'iInter' =>  '',
+		'iInter-diff' =>  '',
+		'qin' =>  '2, 7 X 5, 6, 14',
+		'qpl/trio' =>  [[5, 7, 14, 2, 6]] ,
+		'diff' =>  '',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'Win' =>  '8, 9',
+		'wins' =>  [[8, 9]] ,
+		'inters' =>  [[3, 5, 11]] ,
+		'dInter' =>  '',
+		'dInter-diff' =>  '',
+		'iInter' =>  '',
+		'iInter-diff' =>  '',
+		'qin' =>  '3, 5, 11 X 8, 9',
+		'qpl/trio' =>  [[3, 5, 9, 8, 11]] ,
+		'diff' =>  '',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'Win' =>  '8',
+		'wins' =>  [[8]] ,
+		'inters' =>  [[1, 3, 4, 12]] ,
+		'dInter' =>  '',
+		'dInter-diff' =>  '',
+		'iInter' =>  '',
+		'iInter-diff' =>  '',
+		'qin' =>  '1, 3, 4, 12 X 8',
+		'qpl/trio' =>  [[1, 3, 12, 4, 8]] ,
+		'diff' =>  '',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'wins' =>  [] ,
+		'inters' =>  [[2, 6, 7, 9, 10]] ,
+		'dInter' =>  '',
+		'dInter-diff' =>  '',
+		'iInter' =>  '',
+		'iInter-diff' =>  '',
+		'qin' =>  '2, 6, 7, 9, 10',
+		'qpl/trio' =>  [[7, 9, 2, 6, 10]] ,
+		'diff' =>  '',
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'Win' =>  '6, 7, 9',
+		'wins' =>  [[6, 7, 9]] ,
+		'inters' =>  [[3, 4]] ,
+		'dInter' =>  '',
+		'dInter-diff' =>  '',
+		'iInter' =>  '',
+		'iInter-diff' =>  '',
+		'qin' =>  '3, 4 X 6, 7, 9',
+		'qpl/trio' =>  [[3, 7, 9, 4, 6]] ,
+		'diff' =>  '',
+	],
+];
