0f234d3d2ae0064c9f28da87055c83e36032ba94
artistic3 <artisticsoftware3@gmail.com>
2023-07-29 03:05:42 +0000

    ...

---
+++ 0729/probas.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'qpl' => '7 X 1, 2, 3',
+		//If qpl payout > 100 HKD.
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'qpl' => '6 X 2, 3, 10',
+		//If qpl payout > 50 HKD.
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'qpl' => '10 X 3, 6, 13, 17',
+		//If qpl payout > 100 HKD.
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'qpl' => '10 X 4, 6, 7, 11',
+		//If qpl payout > 50 HKD.
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'qpl' => '3 X 2, 4, 6, 7, 8',
+		//If qpl payout > 50 HKD.
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'qpl' => '10 X 1, 5',
+		//If qpl payout > 100 HKD.
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'qpl' => '8 X 1, 2, 3, 4, 6, 10',
+		//If qpl payout > 100 HKD.
+	],
+];
