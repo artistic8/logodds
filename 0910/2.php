335bc0ec36816b247480ebd074c01cd839741534
artistic3 <artisticsoftware3@gmail.com>
2023-09-09 06:57:26 +0000

    ...

--- 0910/2.php
+++ 0910/2.php
@@ -48,7 +48,7 @@ return [
 		'Sure Place'    =>  '',
 		'All Wins values'    =>  '4, 10',
 		'Place values'    =>  '1, 3, 7',
-		'Red QPL values'      =>  '1, 3, 7',
+		'Red QPL values'      =>  '1, 7, 3',
 		'Black QPL values'    =>  '4, 10',
 		'Inter Inters' =>  '1, 3, 7',
 		'Inter QPL' =>  '1, 3, 7, 4, 10',
