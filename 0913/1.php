f3cab0630d525addc60c429d6398a7bb746016fe
artistic3 <artisticsoftware3@gmail.com>
2023-09-13 08:36:06 +0000

    ...

--- 0913/1.php
+++ 0913/1.php
@@ -22,7 +22,7 @@ return [
 		'inters' =>  [[1, 5, 3, 2], [1, 3, 4]] ,
 		'Favorite' =>  '1',
 		'Red QPL values'      =>  '1, 5, 3',
-		'Black QPL values'    =>  '6, 2, 4',
+		'Black QPL values'    =>  '6, 4, 2',
 		'Inter QPL' =>  '1, 5, 3, 6',
 	],
 	'5' => [
@@ -61,7 +61,7 @@ return [
 		'inters' =>  [[3, 9, 6], [6, 2, 3]] ,
 		'Favorite' =>  '2',
 		'Red QPL values'      =>  '5, 3, 9',
-		'Black QPL values'    =>  '2, 6, 4',
+		'Black QPL values'    =>  '6, 2, 4',
 		'Inter QPL' =>  '3, 6, 5, 2',
 	],
 	'8' => [
