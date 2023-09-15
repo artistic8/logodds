4bbfde8a02e342ee33add8cab9abb3c8b858e519
artistic8 <137678663+artistic8@users.noreply.github.com>
2023-09-15 11:09:56 +0800

    Update bets.php

--- bets.php
+++ bets.php
@@ -2,6 +2,9 @@
 
 if(!isset($argv[1])) die("Race Date Not Entered!!\n");
 
+$blacks = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20,
+          22, 24, 26, 28, 29, 31, 33, 35];
+
 $raceDate = trim($argv[1]);
 $currentDir = __DIR__ . DIRECTORY_SEPARATOR . $raceDate;
 
@@ -26,13 +29,17 @@ foreach($oneData as $raceNumber => $oneRaceDate){
             if(count($winsItem) !== 1) $favoritePlace = false;
         }
         if($favoritePlace){
-            $racetext = "\t'$raceNumber' => [\n";
-            $racetext .= "\t\t/**\n";
-            $racetext .= "\t\tRace $raceNumber\n";
-            $racetext .= "\t\t*/\n";
-            $racetext .= "\t\t'Place' =>  '" . $oneData[$raceNumber]["Favorite"] . "',\n";
-            $racetext .= "\t],\n";
-            $outtext .= $racetext;
+            $favorite = $oneData[$raceNumber]["Favorite"];
+            if(in_array($favorite, $blacks)){
+                $racetext = "\t'$raceNumber' => [\n";
+                $racetext .= "\t\t/**\n";
+                $racetext .= "\t\tRace $raceNumber\n";
+                $racetext .= "\t\t*/\n";
+                $racetext .= "\t\t'Place(100)' =>  '" . $favorite . "',\n";
+                $racetext .= "\t\t'Win  (10) ' =>  '" . $favorite . "',\n";
+                $racetext .= "\t],\n";
+                $outtext .= $racetext;
+            }
         }
     }
     if(isset($oneRaceDate["Place"])){
@@ -44,8 +51,8 @@ foreach($oneData as $raceNumber => $oneRaceDate){
         $racetext .= "\t\t/**\n";
         $racetext .= "\t\tRace $raceNumber\n";
         $racetext .= "\t\t*/\n";
-        $racetext .= "\t\t'Place' =>  '" . $placeBet . "',\n";
-        $racetext .= "\t\t'QQPL' =>  '" . $qplBet . "',\n";
+        $racetext .= "\t\t'Place(100)' =>  '" . $placeBet . "',\n";
+        $racetext .= "\t\t'QQPL (10) ' =>  '" . $qplBet . "',\n";
         $racetext .= "\t],\n";
         $outtext .= $racetext;
     }
