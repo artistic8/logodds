65f8c35382d2460b4ceb989cc58dea4bc461b1d8
artistic software <aadel.iimam@gmail.com>
2023-10-14 02:41:41 +0000

    ...

---
+++ twos.php
@@ -0,0 +1,167 @@
+<?php
+
+function numericalValue($n){
+    $tens = intdiv($n, 10);
+    $units = $n - 10 * $tens;
+    return $tens + $units;
+}
+/**
+ * Returns true if array $needle is contained in array of arrays $haystack 
+ */
+function in_my_array($needle, $haystack){
+    foreach($haystack as $comparedTo){
+        $shit1 = array_values($needle);
+        $shit2 = array_values($comparedTo);
+        sort($shit1); 
+        sort($shit2);
+        $count1 = count($shit1);
+        $count2 = count($shit2);
+        if($count1 == $count2){
+            $coincidences = 0;
+            for($compt = 0; $compt < $count1; $compt++) {
+                if($shit1[$compt] == $shit2[$compt]) $coincidences++;
+            }
+            if($coincidences == $count1) return true;
+        }
+    }
+    return false;
+}
+
+if(!isset($argv[1])) die("Race Date Not Entered!!\n");
+
+$step = 3;
+$raceDate = trim($argv[1]);
+$currentDir = __DIR__ . DIRECTORY_SEPARATOR . $raceDate;
+
+$allOdds = include($currentDir . DIRECTORY_SEPARATOR . "getodds.php");
+
+$outFile = $currentDir . DIRECTORY_SEPARATOR . "$step.php";
+if(file_exists($outFile)){
+    $oldData = include($outFile);
+}
+$probas = [];
+
+$reds = [1, 3, 5, 7, 9, 12, 14, 16, 18, 
+         19, 21, 23, 25, 27, 30, 32, 34, 36];
+
+$blacks = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20,
+          22, 24, 26, 28, 29, 31, 33, 35];
+
+$totalRaces = count($allOdds);
+
+for($r=1; $r <= $totalRaces; $r++){
+    if(!isset($allOdds[$r])) continue;
+    $odds = $allOdds[$r];
+    $proba = [];
+    $sum = 0;
+    foreach($odds as $i => $oddsI){
+            $proba[$i] = 100 * (log($oddsI) / $oddsI) / exp(1);
+            $sum += $proba[$i];
+            }
+    foreach($odds as  $i => $oddsI){
+        //adjust to 100 percentage
+        $proba[$i] = round( $proba[$i] * 100 / $sum, 2);
+    }
+    arsort($proba);
+    $probas[$r] = $proba;
+}
+
+
+$outtext = "<?php\n\n";
+$outtext .= "return [\n";
+
+for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
+    if(!isset($probas[$raceNumber])) continue;
+
+    if(isset($oldData)){
+        if(isset($oldData[$raceNumber])){
+            $oldRaceData = $oldData[$raceNumber];
+            if(isset($oldRaceData['wins'])) $oldWINS = $oldRaceData['wins'];
+        }
+    }
+    $racetext = "";
+
+    $tmpArray = $probas[$raceNumber];
+    $runners = array_keys($tmpArray);
+
+    if(count($runners) < 10) continue;
+    
+    $racetext .= "\t'$raceNumber' => [\n";
+    $racetext .= "\t\t/**\n";
+    $racetext .= "\t\tRace $raceNumber\n";
+    $racetext .= "\t\t*/\n";
+
+    $values = array_values($tmpArray);
+
+    $sBlacks = array_values(array_intersect($runners, $blacks));
+    $sReds = array_values(array_intersect($runners, $reds));
+       
+    $first1 = $runners[0];
+
+    if(in_array($first1, $blacks)){
+       $favorites = $sBlacks;
+       $others = $sReds;
+    }
+    else{
+       $favorites = $sReds;
+       $others =$sBlacks;
+    }
+
+    $trio = array_merge(array_slice($favorites, 0, 3), array_slice($others, 0, 2));
+
+    $qplLeftSide = [$favorites[0], $favorites[1], $favorites[2], $others[0], $others[1]];
+    $qplRightSide = [ $others[3], $favorites[count($favorites) - 3], $others[count($others) - 3], end($favorites), end($others) ];
+    $toWin = [];
+    for($indexL = 0; $indexL < count($qplLeftSide); $indexL++) {
+        for($indexR = 0; $indexR < count($qplRightSide); $indexR++) {
+            $left = $qplLeftSide[$indexL];
+            $right = $qplRightSide[$indexR];
+            if( 
+                (abs(numericalValue($left) - numericalValue($right)) <= 2)
+                &&
+                (
+                    (in_array($left, $sReds) && in_array($right, $sBlacks))
+                    || (in_array($left, $sBlacks) && in_array($right, $sReds))
+                )
+            ){
+                if(!in_array($left, $toWin)) $toWin[] = $left;
+                if(!in_array($right, $toWin)) $toWin[] = $right;
+            }
+        }
+    }
+    $difference = array_values(array_unique(array_diff($trio, $toWin)));
+
+    if(isset($oldWINS)) $wins = $oldWINS;
+    else $wins = [];
+
+    if(count($difference) === 2) {
+        if(!in_my_array($difference, $wins)) $wins[] = $difference;
+    } 
+
+    $allWinsValues = [];
+    $WINSText = "[";
+    $someCounter = 0;
+    $someLength = count($wins);
+    foreach($wins as $winsItem){
+        if(count($winsItem) === 2)
+        {
+            $allWinsValues = array_values(array_unique(array_merge($allWinsValues, $winsItem)));
+        }
+        $WINSText .= "[" . implode(", ", $winsItem) . "]";
+        $someCounter ++;
+        if($someCounter < $someLength) $WINSText .= ", ";
+    }
+    $WINSText .= "]";
+
+    $racetext .= "\t\t'wins' =>  $WINSText ,\n";
+    $racetext .= "\t\t'WP' => '" . implode(", ", $allWinsValues) . "',\n";
+    $racetext .= "\t],\n";
+    unset($oldWINS);
+    if(!empty($allWinsValues)){
+        $outtext .= $racetext;
+    }
+}
+
+$outtext .= "];\n";
+
+file_put_contents($outFile, $outtext);
