12025b37de515c83806f528b55e8b4dd7dfcc1b3
artistic3 <artisticsoftware3@gmail.com>
2023-10-17 04:49:33 +0000

    ...

---
+++ 1018/1.php
@@ -0,0 +1,157 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'wins' =>  [] ,
+		'qpl/trio' =>  [[3, 5, 9, 11, 2]] ,
+		'inters' =>  [[3, 5, 9, 11, 2]] ,
+		'Favorite' =>  '3',
+		'Sure Place'    =>  '11, 2',
+		'Red QPL values'      =>  '3, 5, 9',
+		'Black QPL values'    =>  '11, 2',
+		'All QPL values'      =>  '3, 5, 9, 11, 2',
+		'place values'        =>  '3, 5, 9, 11, 2',
+		'Inter QPL' =>  '3, 5, 9, 11, 2',
+		'Diff1' =>  '',
+		'Diff2' =>  '3, 5, 9, 11, 2',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'wins' =>  [[4, 6, 5]] ,
+		'qpl/trio' =>  [[2, 3, 4, 6, 5]] ,
+		'inters' =>  [[2, 3]] ,
+		'Favorite' =>  '4',
+		'Sure Place'    =>  '2',
+		'Red QPL values'      =>  '3, 5',
+		'Black QPL values'    =>  '4, 2, 6',
+		'All QPL values'      =>  '4, 3, 5, 2, 6',
+		'place values'        =>  '3, 2',
+		'Inter QPL' =>  '2, 3, 4, 6, 5',
+		'Diff1' =>  '',
+		'Diff2' =>  '2, 3',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'wins' =>  [[9]] ,
+		'qpl/trio' =>  [[1, 5, 4, 2, 9]] ,
+		'inters' =>  [[1, 5, 4, 2]] ,
+		'Favorite' =>  '1',
+		'Sure Place'    =>  '4, 2',
+		'Red QPL values'      =>  '1, 5, 9',
+		'Black QPL values'    =>  '4, 2',
+		'All QPL values'      =>  '1, 5, 4, 2, 9',
+		'place values'        =>  '1, 5, 4, 2',
+		'Inter QPL' =>  '1, 5, 4, 2, 9',
+		'Diff1' =>  '',
+		'Diff2' =>  '1, 5, 4, 2',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'wins' =>  [] ,
+		'qpl/trio' =>  [[2, 4, 10, 3, 7]] ,
+		'inters' =>  [[2, 4, 10, 3, 7]] ,
+		'Favorite' =>  '2',
+		'Sure Place'    =>  '2',
+		'Red QPL values'      =>  '3, 7',
+		'Black QPL values'    =>  '2, 4, 10',
+		'All QPL values'      =>  '2, 3, 4, 7, 10',
+		'place values'        =>  '2, 3, 4, 7, 10',
+		'Inter QPL' =>  '2, 4, 10, 3, 7',
+		'Diff1' =>  '',
+		'Diff2' =>  '2, 4, 10, 3, 7',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'wins' =>  [[5, 4]] ,
+		'qpl/trio' =>  [[1, 12, 8, 5, 4]] ,
+		'inters' =>  [[1, 12, 8]] ,
+		'Favorite' =>  '1',
+		'Sure Place'    =>  '8',
+		'Red QPL values'      =>  '1, 12, 5',
+		'Black QPL values'    =>  '8, 4',
+		'All QPL values'      =>  '1, 8, 4, 12, 5',
+		'place values'        =>  '1, 8, 12',
+		'Inter QPL' =>  '1, 12, 8, 5, 4',
+		'Diff1' =>  '',
+		'Diff2' =>  '1, 12, 8',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'wins' =>  [[9]] ,
+		'qpl/trio' =>  [[6, 11, 2, 3, 9]] ,
+		'inters' =>  [[6, 11, 2, 3]] ,
+		'Favorite' =>  '6',
+		'Sure Place'    =>  '6',
+		'Red QPL values'      =>  '3, 9',
+		'Black QPL values'    =>  '6, 11, 2',
+		'All QPL values'      =>  '6, 3, 11, 9, 2',
+		'place values'        =>  '6, 3, 11, 2',
+		'Inter QPL' =>  '6, 11, 2, 3, 9',
+		'Diff1' =>  '',
+		'Diff2' =>  '6, 11, 2, 3',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'wins' =>  [] ,
+		'qpl/trio' =>  [[2, 4, 10, 7, 3]] ,
+		'inters' =>  [[2, 4, 10, 7, 3]] ,
+		'Favorite' =>  '2',
+		'Sure Place'    =>  '2',
+		'Red QPL values'      =>  '7, 3',
+		'Black QPL values'    =>  '2, 4, 10',
+		'All QPL values'      =>  '2, 4, 10, 7, 3',
+		'place values'        =>  '2, 4, 10, 7, 3',
+		'Inter QPL' =>  '2, 4, 10, 7, 3',
+		'Diff1' =>  '',
+		'Diff2' =>  '2, 4, 10, 7, 3',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'wins' =>  [[6]] ,
+		'qpl/trio' =>  [[3, 1, 7, 11, 6]] ,
+		'inters' =>  [[3, 1, 7, 11]] ,
+		'Favorite' =>  '3',
+		'Sure Place'    =>  '11',
+		'Red QPL values'      =>  '3, 1, 7',
+		'Black QPL values'    =>  '6, 11',
+		'All QPL values'      =>  '3, 1, 6, 7, 11',
+		'place values'        =>  '3, 1, 7, 11',
+		'Inter QPL' =>  '3, 1, 7, 11, 6',
+		'Diff1' =>  '',
+		'Diff2' =>  '3, 1, 7, 11',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'wins' =>  [[5]] ,
+		'qpl/trio' =>  [[1, 3, 6, 8, 5]] ,
+		'inters' =>  [[1, 3, 6, 8]] ,
+		'Favorite' =>  '1',
+		'Sure Place'    =>  '6, 8',
+		'Red QPL values'      =>  '1, 5, 3',
+		'Black QPL values'    =>  '6, 8',
+		'All QPL values'      =>  '1, 5, 6, 8, 3',
+		'place values'        =>  '1, 6, 8, 3',
+		'Inter QPL' =>  '1, 3, 6, 8, 5',
+		'Diff1' =>  '',
+		'Diff2' =>  '1, 3, 6, 8',
+	],
+];
