5de8b4fb0bdcd47935e7d464fd585f7b2d46d3aa
artistic software <aadel.iimam@gmail.com>
2023-10-21 10:34:50 +0000

    ...

--- 1022/1.php
+++ 1022/1.php
@@ -58,7 +58,7 @@ return [
 		*/
 		'wins' =>  [[9]] ,
 		'qpl/trio' =>  [[3, 14, 8, 10, 9], [14, 3, 8, 2, 9]] ,
-		'All QPL values'      =>  '14, 3, 9, 8, 2, 10',
+		'All QPL values'      =>  '14, 9, 3, 8, 2, 10',
 		'new 2 qpl/trio' =>  [] ,
 		'new 3 qpl/trio' =>  [] ,
 		'New 2 QPL values' =>  '',
@@ -92,7 +92,7 @@ return [
 		*/
 		'wins' =>  [[1], [7]] ,
 		'qpl/trio' =>  [[2, 13, 6, 7, 1], [2, 13, 6, 3, 7], [2, 13, 6, 3, 1]] ,
-		'All QPL values'      =>  '2, 3, 7, 1, 13, 6',
+		'All QPL values'      =>  '2, 3, 1, 7, 13, 6',
 		'new 2 qpl/trio' =>  [] ,
 		'new 3 qpl/trio' =>  [] ,
 		'New 2 QPL values' =>  '',
@@ -109,7 +109,7 @@ return [
 		*/
 		'wins' =>  [[9]] ,
 		'qpl/trio' =>  [[11, 2, 6, 3, 9], [11, 2, 6, 12, 9]] ,
-		'All QPL values'      =>  '11, 9, 2, 6, 12, 3',
+		'All QPL values'      =>  '11, 9, 2, 6, 3, 12',
 		'new 2 qpl/trio' =>  [] ,
 		'new 3 qpl/trio' =>  [] ,
 		'New 2 QPL values' =>  '',
@@ -124,13 +124,13 @@ return [
 		/**
 		Race 9
 		*/
-		'wins' =>  [[7, 9, 8], [7, 8]] ,
+		'wins' =>  [[7, 9, 8], [7, 8], [7]] ,
 		'qpl/trio' =>  [[1, 6, 7, 9, 8], [1, 13, 7, 9, 8], [1, 5, 13, 7, 8]] ,
-		'All QPL values'      =>  '1, 7, 8, 5, 9, 13, 6',
+		'All QPL values'      =>  '1, 7, 8, 5, 13, 9, 6',
 		'new 2 qpl/trio' =>  [[1, 6, 7, 9, 8], [1, 13, 7, 9, 8], [1, 5, 13, 7, 8]] ,
 		'new 3 qpl/trio' =>  [[1, 6, 7, 9, 8], [1, 13, 7, 9, 8]] ,
-		'New 2 QPL values' =>  '1, 7, 8, 5, 9, 13, 6',
-		'New 3 QPL values' =>  '1, 7, 8, 9, 13, 6',
+		'New 2 QPL values' =>  '1, 7, 8, 5, 13, 9, 6',
+		'New 3 QPL values' =>  '1, 7, 8, 13, 9, 6',
 		'Tce'            =>  '1, 5, 7, 8, 9, 13',
 		'diff1'          =>  '5',
 		'diff2'          =>  '',
