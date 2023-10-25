c915381a5d2141fc0f1c239c34e80dcf29048d88
artistic software <aadel.iimam@gmail.com>
2023-10-25 00:13:16 +0000

    ...

--- 1025/1.php
+++ 1025/1.php
@@ -6,13 +6,13 @@ return [
 		Race 1
 		*/
 		'wins' =>  [[6]] ,
-		'qpl/trio'       =>  [[5, 3, 7, 11, 6]] ,
+		'qpl/trio'       =>  [[5, 3, 7, 11, 6], [5, 3, 7, 2, 6]] ,
 		'new 2 qpl/trio' =>  [] ,
 		'new 3 qpl/trio' =>  [] ,
-		'All QPL values'    =>  '5, 6, 11, 3, 7',
+		'All QPL values'    =>  '5, 6, 2, 3, 11, 7',
 		'New 2 QPL values'  =>  '',
 		'New 3 QPL values'  =>  '',
-		'Tce'               =>  '3, 5, 6, 7, 11',
+		'Tce'               =>  '2, 3, 5, 6, 7, 11',
 		/**Method 1: If allQplValues === 2QplValues and empty(3QplValues) then place favourite, 
      Method 2: Remove I from first 5 elts of Tce, if favourite is one of them then place favourite*/
 		'I' =>  '',
@@ -25,9 +25,9 @@ return [
 		'qpl/trio'       =>  [[7, 3, 11, 9, 8]] ,
 		'new 2 qpl/trio' =>  [[7, 3, 11, 9, 8]] ,
 		'new 3 qpl/trio' =>  [[7, 3, 11, 9, 8]] ,
-		'All QPL values'    =>  '7, 8, 11, 9, 3',
-		'New 2 QPL values'  =>  '7, 8, 11, 9, 3',
-		'New 3 QPL values'  =>  '7, 8, 11, 9, 3',
+		'All QPL values'    =>  '7, 11, 8, 9, 3',
+		'New 2 QPL values'  =>  '7, 11, 8, 9, 3',
+		'New 3 QPL values'  =>  '7, 11, 8, 9, 3',
 		'Tce'               =>  '3, 7, 8, 9, 11',
 		/**Method 1: If allQplValues === 2QplValues and empty(3QplValues) then place favourite, 
      Method 2: Remove I from first 5 elts of Tce, if favourite is one of them then place favourite*/
@@ -38,13 +38,13 @@ return [
 		Race 3
 		*/
 		'wins' =>  [[7, 2], [7]] ,
-		'qpl/trio'       =>  [[12, 1, 6, 7, 2], [12, 5, 6, 7, 2]] ,
-		'new 2 qpl/trio' =>  [[12, 1, 6, 7, 2], [12, 5, 6, 7, 2]] ,
+		'qpl/trio'       =>  [[12, 1, 6, 7, 2], [12, 5, 6, 7, 2], [6, 2, 8, 12, 7]] ,
+		'new 2 qpl/trio' =>  [[12, 1, 6, 7, 2], [12, 5, 6, 7, 2], [6, 2, 8, 12, 7]] ,
 		'new 3 qpl/trio' =>  [] ,
-		'All QPL values'    =>  '7, 6, 12, 2, 5, 1',
-		'New 2 QPL values'  =>  '7, 6, 12, 2, 5, 1',
+		'All QPL values'    =>  '6, 7, 12, 2, 5, 8, 1',
+		'New 2 QPL values'  =>  '6, 7, 12, 2, 5, 8, 1',
 		'New 3 QPL values'  =>  '',
-		'Tce'               =>  '1, 2, 5, 6, 7, 12',
+		'Tce'               =>  '2, 5, 6, 7, 8, 12',
 		/**Method 1: If allQplValues === 2QplValues and empty(3QplValues) then place favourite, 
      Method 2: Remove I from first 5 elts of Tce, if favourite is one of them then place favourite*/
 		'I' =>  '6, 12, 5',
@@ -86,13 +86,13 @@ return [
 		Race 6
 		*/
 		'wins' =>  [[11]] ,
-		'qpl/trio'       =>  [[3, 1, 12, 8, 4], [3, 1, 12, 8, 11]] ,
+		'qpl/trio'       =>  [[3, 1, 12, 8, 4], [3, 1, 12, 8, 11], [3, 12, 14, 8, 4]] ,
 		'new 2 qpl/trio' =>  [] ,
 		'new 3 qpl/trio' =>  [] ,
-		'All QPL values'    =>  '3, 8, 12, 1, 11, 4',
+		'All QPL values'    =>  '3, 8, 12, 14, 4, 1, 11',
 		'New 2 QPL values'  =>  '',
 		'New 3 QPL values'  =>  '',
-		'Tce'               =>  '1, 3, 4, 8, 11, 12',
+		'Tce'               =>  '1, 3, 4, 8, 12, 14',
 		/**Method 1: If allQplValues === 2QplValues and empty(3QplValues) then place favourite, 
      Method 2: Remove I from first 5 elts of Tce, if favourite is one of them then place favourite*/
 		'I' =>  '',
@@ -121,8 +121,8 @@ return [
 		'qpl/trio'       =>  [[1, 12, 4, 6, 5]] ,
 		'new 2 qpl/trio' =>  [[1, 12, 4, 6, 5]] ,
 		'new 3 qpl/trio' =>  [] ,
-		'All QPL values'    =>  '5, 4, 6, 12, 1',
-		'New 2 QPL values'  =>  '5, 4, 6, 12, 1',
+		'All QPL values'    =>  '5, 4, 12, 1, 6',
+		'New 2 QPL values'  =>  '5, 4, 12, 1, 6',
 		'New 3 QPL values'  =>  '',
 		'Tce'               =>  '1, 4, 5, 6, 12',
 		/**Method 1: If allQplValues === 2QplValues and empty(3QplValues) then place favourite, 
