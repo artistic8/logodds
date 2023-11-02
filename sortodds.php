c7def0e762d350dab55ce50b193ab7d914537218
artistic software <aadel.iimam@gmail.com>
2023-11-02 02:22:18 +0000

    ...

--- sortodds.php
+++ sortodds.php
@@ -225,8 +225,8 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
     $forReference = array_diff($allQplValues, $allWinsValues);
     $weird = array_diff($runners, $allQplValues);
 
-    $racetext .= "\t\t'all wins values'  =>  '" . implode(", ", $allWinsValues). " //count: " . count($allWinsValues) . "',\n";
-    $racetext .= "\t\t'for reference  '  =>  '" . implode(", ", $forReference). "',\n";
+    $racetext .= "\t\t'all wins values'  =>  '" . implode(", ", $allWinsValues). " //count wins: " . count($allWinsValues) . "',\n";
+    $racetext .= "\t\t'for reference  '  =>  '" . implode(", ", $forReference). " //count ref: " . count($forReference) . "',\n";
     $racetext .= "\t\t'weird values   '  =>  '" . implode(", ", $weird). "',\n";
     
     if(!empty($allWinsValues)){
