4dcece8bd2397f0ef0fa4350352ab5e9bffd4d42
artistic software <aadel.iimam@gmail.com>
2023-11-08 05:27:37 +0000

    ...

--- 1108/1.php
+++ 1108/1.php
@@ -10,6 +10,7 @@ return [
 		'All Runners   '  =>  '8, 7, 5, 2, 6, 11, 1, 9, 4, 3, 10, 12',
 		'All QPL values'  =>  '8, 7, 5, 2, 6',
 		'All Wins'        =>  '7, 5, 6',
+		'Reference'        =>  '8, 2',
 		'favorite' =>  '8',
 	],
 	'2' => [
@@ -20,7 +21,9 @@ return [
 		'qpl/trio'       =>  [[2, 11, 10, 5, 3]] ,
 		'All Runners   '  =>  '2, 11, 3, 5, 1, 10, 9, 7, 8, 12, 4, 6',
 		'All QPL values'  =>  '2, 11, 3, 5, 10',
+		//All wins contains 3
 		'All Wins'        =>  '3',
+		'Reference'        =>  '2, 11, 5, 10',
 		'favorite' =>  '2',
 	],
 	'3' => [
@@ -32,6 +35,8 @@ return [
 		'All Runners   '  =>  '3, 8, 2, 1, 12, 9, 10, 6, 11, 7, 5, 4',
 		'All QPL values'  =>  '3, 8, 2, 1, 12',
 		'All Wins'        =>  '2, 1',
+		//Reference contains 3
+		'Reference'        =>  '3, 8, 12',
 		'favorite' =>  '3',
 	],
 	'4' => [
@@ -43,6 +48,7 @@ return [
 		'All Runners   '  =>  '1, 9, 4, 6, 8, 11, 2, 12, 5, 3, 7, 10',
 		'All QPL values'  =>  '1, 9, 4, 6, 12',
 		'All Wins'        =>  '9',
+		'Reference'        =>  '1, 4, 6, 12',
 		'favorite' =>  '1',
 	],
 	'5' => [
@@ -54,6 +60,7 @@ return [
 		'All Runners   '  =>  '8, 7, 12, 6, 2, 5, 9, 3, 11, 1, 4, 10',
 		'All QPL values'  =>  '8, 7, 12, 6, 2',
 		'All Wins'        =>  '7, 6',
+		'Reference'        =>  '8, 12, 2',
 		'favorite' =>  '8',
 	],
 	'6' => [
@@ -65,6 +72,7 @@ return [
 		'All Runners   '  =>  '1, 12, 7, 4, 2, 9, 5, 11, 8, 3, 10, 6',
 		'All QPL values'  =>  '1, 12, 7, 4, 2',
 		'All Wins'        =>  '1, 12',
+		'Reference'        =>  '7, 4, 2',
 		'favorite' =>  '1',
 	],
 	'7' => [
@@ -76,6 +84,7 @@ return [
 		'All Runners   '  =>  '4, 8, 10, 1, 7, 11, 6, 3, 5, 9, 2, 12',
 		'All QPL values'  =>  '4, 8, 10, 1, 7',
 		'All Wins'        =>  '8, 7',
+		'Reference'        =>  '4, 10, 1',
 		'favorite' =>  '4',
 	],
 	'8' => [
@@ -84,9 +93,10 @@ return [
 		*/
 		'wins' =>  [] ,
 		'qpl/trio'       =>  [[1, 7, 9, 2, 6]] ,
-		'All Runners   '  =>  '1, 7, 2, 6, 4, 9, 11, 10, 5, 8, 12, 3',
+		'All Runners   '  =>  '1, 7, 2, 6, 4, 9, 10, 11, 5, 8, 12, 3',
 		'All QPL values'  =>  '1, 7, 2, 6, 9',
 		'All Wins'        =>  '',
+		'Reference'        =>  '1, 7, 2, 6, 9',
 		'favorite' =>  '1',
 	],
 	'9' => [
@@ -98,6 +108,8 @@ return [
 		'All Runners   '  =>  '10, 11, 3, 1, 8, 4, 5, 12, 2, 6, 7, 9',
 		'All QPL values'  =>  '10, 11, 3, 1, 8',
 		'All Wins'        =>  '1',
+		//Reference contains 3
+		'Reference'        =>  '10, 11, 3, 8',
 		'favorite' =>  '10',
 	],
 ];
