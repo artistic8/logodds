2380df1f0015f4fa56899b0a39d8851fa54a3f77
artistic software <aadel.iimam@gmail.com>
2023-11-09 06:38:54 +0000

    ...

---
+++ archive/0628/1.php
@@ -0,0 +1,103 @@
+<?php
+
+return [
+	'2' => [
+		/**
+		Race 2
+		*/
+		'wins' =>  [[9, 8]] ,
+		'qpl/trio'       =>  [[3, 1, 11, 9, 8]] ,
+		'All Runners   '  =>  '3, 1, 9, 5, 11, 7, 8, 12, 6, 2, 10, 4',
+		'All QPL values'  =>  '3, 1, 9, 11, 8',
+		'All Wins'        =>  '9, 8',
+		//Reference contains 3
+		'Reference'        =>  '3, 1, 11',
+		'favorite' =>  '3',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'wins' =>  [[9]] ,
+		'qpl/trio'       =>  [[12, 5, 2, 8, 9]] ,
+		'All Runners   '  =>  '12, 9, 5, 3, 1, 2, 8, 10, 4, 7, 11, 6',
+		'All QPL values'  =>  '12, 9, 5, 2, 8',
+		'All Wins'        =>  '9',
+		'Reference'        =>  '12, 5, 2, 8',
+		'favorite' =>  '12',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'wins' =>  [[9, 10]] ,
+		'qpl/trio'       =>  [[1, 12, 6, 9, 10]] ,
+		'All Runners   '  =>  '9, 1, 10, 6, 12, 8, 4, 7, 3, 2, 11, 5',
+		'All QPL values'  =>  '9, 1, 10, 6, 12',
+		'All Wins'        =>  '9, 10',
+		'Reference'        =>  '1, 6, 12',
+		'favorite' =>  '9',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'wins' =>  [] ,
+		'qpl/trio'       =>  [[1, 5, 3, 10, 11]] ,
+		'All Runners   '  =>  '1, 5, 3, 10, 11, 6, 7, 2, 9, 8, 12, 4',
+		'All QPL values'  =>  '1, 5, 3, 10, 11',
+		'All Wins'        =>  '',
+		//Reference contains 3
+		'Reference'        =>  '1, 5, 3, 10, 11',
+		'favorite' =>  '1',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'wins' =>  [[7]] ,
+		'qpl/trio'       =>  [[6, 8, 4, 12, 7]] ,
+		'All Runners   '  =>  '6, 7, 8, 12, 4, 3, 2, 9, 11, 10, 1, 5',
+		'All QPL values'  =>  '6, 7, 8, 12, 4',
+		'All Wins'        =>  '7',
+		'Reference'        =>  '6, 8, 12, 4',
+		'favorite' =>  '6',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'wins' =>  [[1]] ,
+		'qpl/trio'       =>  [[5, 7, 11, 4, 1]] ,
+		'All Runners   '  =>  '1, 5, 11, 4, 2, 7, 6, 10, 8, 12, 9, 3',
+		'All QPL values'  =>  '1, 5, 11, 4, 7',
+		'All Wins'        =>  '1',
+		'Reference'        =>  '5, 11, 4, 7',
+		'favorite' =>  '1',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'wins' =>  [[6, 8, 5, 9]] ,
+		'qpl/trio'       =>  [[4, 6, 8, 5, 9]] ,
+		'All Runners   '  =>  '4, 6, 5, 9, 7, 8, 1, 3, 12, 2, 11, 10',
+		'All QPL values'  =>  '4, 6, 5, 9, 8',
+		'All Wins'        =>  '6, 5, 9, 8',
+		'Reference'        =>  '4',
+		'favorite' =>  '4',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'wins' =>  [] ,
+		'qpl/trio'       =>  [[2, 6, 11, 1, 3]] ,
+		'All Runners   '  =>  '2, 6, 1, 3, 9, 11, 12, 5, 10, 7, 8, 4',
+		'All QPL values'  =>  '2, 6, 1, 3, 11',
+		'All Wins'        =>  '',
+		//Reference contains 3
+		'Reference'        =>  '2, 6, 1, 3, 11',
+		'favorite' =>  '2',
+	],
+];
