35fb9b83029e3d099838147124120fb7e61cd5b2
artistic software <aadel.iimam@gmail.com>
2023-11-13 02:34:12 +0000

    weights.php

--- weights.php
+++ weights.php
@@ -6,26 +6,33 @@ $odds = [
     9 => 24,
     12 => 13
 ];
-$weights = [];
-$totalWeights = 0;
-foreach($odds as $key => $value){
-    $weights[$key] = 1;
-    $totalWeights += $weights[$key];
-}
-$criterion = true;
-foreach($odds as $key => $value){
-    $criterion = $criterion && ($weights[$key] * $odds[$key] >= $totalWeights);
-}
-while($criterion === false){
+function getWeights($odds, $profit = 0, $precision = 10){
+    $weights = [];
+    $totalWeights = 0;
+    foreach($odds as $key => $value){
+        $weights[$key] = 1;
+        $totalWeights += $weights[$key];
+    }
     $criterion = true;
     foreach($odds as $key => $value){
-        if($weights[$key] * $odds[$key] < $totalWeights){
-            $weights[$key] +=1;
-            $totalWeights += 1;
-        }
-        $criterion = $criterion && ($weights[$key] * $odds[$key] >= $totalWeights);
+        $criterion = $criterion && ($weights[$key] * $odds[$key] >= $totalWeights + $profit);
     }
+    $iterations = 0;
+    while($criterion === false){
+        $criterion = true;
+        foreach($odds as $key => $value){
+            if($weights[$key] * $odds[$key] < $totalWeights + $profit){
+                $weights[$key] +=1;
+                $totalWeights += 1;
+            }
+            $criterion = $criterion && ($weights[$key] * $odds[$key] >= $totalWeights + $profit);
+        }
+        $iterations ++;
+        if($iterations == $precision) return array_fill(0, count($odds), -1);
+    }
+    return $weights;
 }
+$weights = getWeights($odds, 2, 10);
 var_dump($weights);
-var_dump($totalWeights); die();
+die();
 ?>
