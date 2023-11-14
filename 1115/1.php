b7cc77efc1fa19d9a2786862bdede5594384edea
artistic software <aadel.iimam@gmail.com>
2023-11-14 08:37:21 +0000

    ...

---
+++ 1115/1.php
@@ -0,0 +1,189 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'wins' =>  [[2, 4]] ,
+		'qpl/trio'       =>  [[3, 12, 1, 2, 4]] ,
+		'All Runners   '  =>  '10, 3, 2, 4, 12, 1, 11, 7, 5, 9, 6, 8',
+		'All QPL values'  =>  '3, 2, 4, 12, 1',
+		'All Wins'        =>  '2, 4',
+		//Reference contains 3
+		'Reference'        =>  '3, 12, 1',
+		'favorite' =>  '3',
+		'WIN BETS' => [
+			3 =>  20,
+			2 =>  20,
+			4 =>  20,
+			12 =>  20,
+			1 =>  20,
+			11 =>  20,
+			7 =>  20,
+			9 =>  10,
+			5 =>  10,
+			6 =>  10,
+		],
+		//Total bets:170',
+		//count:10',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'wins' =>  [[1]] ,
+		'qpl/trio'       =>  [[11, 2, 6, 3, 1]] ,
+		'All Runners   '  =>  '11, 2, 1, 3, 9, 6, 8, 4, 12, 5, 10, 7',
+		'All QPL values'  =>  '11, 2, 1, 3, 6',
+		'All Wins'        =>  '1',
+		//Reference contains 3
+		'Reference'        =>  '11, 2, 3, 6',
+		'favorite' =>  '11',
+		'WIN BETS' => [
+			11 =>  60,
+			2 =>  40,
+			1 =>  30,
+			9 =>  20,
+			3 =>  20,
+		],
+		//Total bets:170',
+		//count:5',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'wins' =>  [[4, 2]] ,
+		'qpl/trio'       =>  [[1, 11, 3, 4, 2]] ,
+		'All Runners   '  =>  '4, 1, 3, 2, 5, 11, 9, 8, 10, 6, 12, 7',
+		'All QPL values'  =>  '4, 1, 3, 2, 11',
+		'All Wins'        =>  '4, 2',
+		//Reference contains 3
+		'Reference'        =>  '1, 3, 11',
+		'favorite' =>  '4',
+		'WIN BETS' => [
+			4 =>  80,
+			1 =>  70,
+			3 =>  60,
+			2 =>  60,
+			11 =>  50,
+			9 =>  40,
+			8 =>  40,
+		],
+		//Total bets:400',
+		//count:7',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'wins' =>  [[6]] ,
+		'qpl/trio'       =>  [[2, 8, 1, 12, 6]] ,
+		'All Runners   '  =>  '2, 8, 6, 1, 12, 5, 4, 10, 11, 9, 7, 3',
+		'All QPL values'  =>  '2, 8, 6, 1, 12',
+		'All Wins'        =>  '6',
+		'Reference'        =>  '2, 8, 1, 12',
+		'favorite' =>  '2',
+		'WIN BETS' => [
+			2 =>  90,
+			8 =>  40,
+			6 =>  30,
+			1 =>  30,
+			12 =>  20,
+		],
+		//Total bets:210',
+		//count:5',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'wins' =>  [[4]] ,
+		'qpl/trio'       =>  [[1, 3, 12, 8, 4]] ,
+		'All Runners   '  =>  '1, 4, 3, 8, 6, 12, 9, 5, 2, 7, 10, 11',
+		'All QPL values'  =>  '1, 4, 3, 8, 12',
+		'All Wins'        =>  '4',
+		//Reference contains 3
+		'Reference'        =>  '1, 3, 8, 12',
+		'favorite' =>  '1',
+		'WIN BETS' => [
+			1 =>  90,
+			4 =>  60,
+			3 =>  60,
+			8 =>  60,
+			6 =>  40,
+		],
+		//Total bets:310',
+		//count:5',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'wins' =>  [[3]] ,
+		'qpl/trio'       =>  [[4, 8, 2, 9, 3]] ,
+		'All Runners   '  =>  '4, 9, 8, 3, 10, 1, 2, 6, 7, 5',
+		'All QPL values'  =>  '4, 9, 8, 3, 2',
+		//All wins contains 3
+		'All Wins'        =>  '3',
+		'Reference'        =>  '4, 9, 8, 2',
+		'favorite' =>  '4',
+		'WIN BETS' => [
+			4 =>  100,
+			9 =>  100,
+			8 =>  90,
+			3 =>  50,
+			2 =>  30,
+			6 =>  30,
+		],
+		//Total bets:400',
+		//count:6',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'wins' =>  [[2, 3, 1]] ,
+		'qpl/trio'       =>  [[4, 8, 2, 3, 1]] ,
+		'All Runners   '  =>  '4, 3, 1, 12, 2, 5, 8, 10, 7, 9, 11, 6',
+		'All QPL values'  =>  '4, 3, 1, 2, 8',
+		//All wins contains 3
+		'All Wins'        =>  '3, 1, 2',
+		'Reference'        =>  '4, 8',
+		'favorite' =>  '4',
+		'WIN BETS' => [
+			4 =>  80,
+			3 =>  70,
+			1 =>  30,
+			2 =>  20,
+			8 =>  20,
+		],
+		//Total bets:220',
+		//count:5',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'wins' =>  [] ,
+		'qpl/trio'       =>  [[6, 11, 4, 9, 3]] ,
+		'All Runners   '  =>  '6, 11, 4, 9, 2, 3, 5, 12, 1, 7, 8, 10',
+		'All QPL values'  =>  '6, 11, 4, 9, 3',
+		'All Wins'        =>  '',
+		//Reference contains 3
+		'Reference'        =>  '6, 11, 4, 9, 3',
+		'favorite' =>  '6',
+		'WIN BETS' => [
+			6 =>  100,
+			11 =>  40,
+			4 =>  40,
+			9 =>  40,
+			3 =>  20,
+			1 =>  20,
+			7 =>  20,
+		],
+		//Total bets:280',
+		//count:7',
+	],
+];
