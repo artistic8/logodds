0b4df64c6dd918e5f4067060aecfb30cb7b4057f
artistic3 <artisticsoftware3@gmail.com>
2023-11-16 02:35:31 +0000

    ...

--- 1115/1.php
+++ 1115/1.php
@@ -5,56 +5,59 @@ return [
 		/**
 		Race 1
 		*/
-		'wins' =>  [[1, 2, 11]] ,
-		'qpl/trio'       =>  [[12, 3, 1, 2, 11]] ,
 		'All Runners   '  =>  '10, 2, 12, 3, 11, 4, 1, 7, 5, 9, 8, 6',
-		'All QPL values'  =>  '2, 12, 3, 11, 1',
-		'All Wins'        =>  '2, 11, 1',
-		//Reference contains 3
-		'Reference'        =>  '12, 3',
 		'favorite' =>  '10',
 		'WIN BETS' => [
-			1 =>  10,
-			4 =>  10,
+			1 =>  20,
+			4 =>  20,
 			6 =>  10,
-			7 =>  10,
-			10 =>  50,
+			7 =>  20,
+			8 =>  10,
+			9 =>  10,
+			10 =>  90,
 		],
-		//Total bets:90',
-		//count:5',
+		//Total bets:180',
+		//count:7',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'All Runners   '  =>  '4, 2, 1, 6, 7, 8, 5, 3',
+		'favorite' =>  '4',
+		'WIN BETS' => [
+			1 =>  -10,
+			2 =>  -10,
+			3 =>  -10,
+			4 =>  -10,
+			6 =>  -10,
+		],
+		//Total bets:-50',
+		//count:7',
 	],
 	'3' => [
 		/**
 		Race 3
 		*/
-		'wins' =>  [[1]] ,
-		'qpl/trio'       =>  [[11, 2, 6, 9, 1]] ,
 		'All Runners   '  =>  '11, 2, 1, 9, 3, 6, 4, 12, 5, 8, 10, 7',
-		'All QPL values'  =>  '11, 2, 1, 9, 6',
-		'All Wins'        =>  '1',
-		'Reference'        =>  '11, 2, 9, 6',
 		'favorite' =>  '11',
 		'WIN BETS' => [
-			1 =>  20,
-			4 =>  10,
+			1 =>  30,
+			3 =>  20,
+			4 =>  20,
 			5 =>  10,
 			7 =>  10,
 			8 =>  10,
+			11 =>  50,
 		],
-		//Total bets:60',
-		//count:5',
+		//Total bets:150',
+		//count:7',
 	],
 	'4' => [
 		/**
 		Race 4
 		*/
-		'wins' =>  [[11]] ,
-		'qpl/trio'       =>  [[1, 3, 9, 4, 11]] ,
 		'All Runners   '  =>  '1, 4, 11, 2, 3, 9, 5, 6, 8, 10, 12, 7',
-		'All QPL values'  =>  '1, 4, 11, 3, 9',
-		'All Wins'        =>  '11',
-		//Reference contains 3
-		'Reference'        =>  '1, 4, 3, 9',
 		'favorite' =>  '1',
 		'WIN BETS' => [
 			1 =>  20,
@@ -62,116 +65,98 @@ return [
 			5 =>  10,
 			6 =>  10,
 			7 =>  10,
+			10 =>  10,
+			12 =>  10,
 		],
-		//Total bets:70',
-		//count:5',
+		//Total bets:90',
+		//count:7',
 	],
 	'5' => [
 		/**
 		Race 5
 		*/
-		'wins' =>  [] ,
-		'qpl/trio'       =>  [[2, 8, 11, 1, 12]] ,
 		'All Runners   '  =>  '2, 1, 8, 12, 5, 11, 9, 4, 10, 7, 3, 6',
-		'All QPL values'  =>  '2, 8, 1, 12, 11',
-		'All Wins'        =>  '',
-		'Reference'        =>  '2, 8, 1, 12, 11',
 		'favorite' =>  '2',
 		'WIN BETS' => [
-			2 =>  40,
+			2 =>  60,
 			3 =>  10,
-			5 =>  10,
+			4 =>  10,
+			5 =>  20,
 			6 =>  10,
-			12 =>  10,
+			10 =>  10,
+			12 =>  20,
 		],
-		//Total bets:80',
-		//count:5',
+		//Total bets:140',
+		//count:7',
 	],
 	'6' => [
 		/**
 		Race 6
 		*/
-		'wins' =>  [[4]] ,
-		'qpl/trio'       =>  [[1, 3, 12, 6, 4]] ,
 		'All Runners   '  =>  '1, 3, 4, 6, 8, 12, 9, 5, 2, 7, 10, 11',
-		'All QPL values'  =>  '1, 3, 6, 4, 12',
-		'All Wins'        =>  '4',
-		//Reference contains 3
-		'Reference'        =>  '1, 3, 6, 12',
 		'favorite' =>  '1',
 		'WIN BETS' => [
-			1 =>  30,
+			1 =>  50,
 			2 =>  10,
 			5 =>  10,
-			6 =>  20,
+			6 =>  40,
 			7 =>  10,
+			10 =>  10,
+			12 =>  20,
 		],
-		//Total bets:80',
-		//count:5',
+		//Total bets:150',
+		//count:7',
 	],
 	'7' => [
 		/**
 		Race 7
 		*/
-		'wins' =>  [] ,
-		'qpl/trio'       =>  [[4, 8, 6, 9, 3]] ,
 		'All Runners   '  =>  '4, 8, 9, 10, 3, 1, 6, 2, 7, 5',
-		'All QPL values'  =>  '4, 8, 9, 3, 6',
-		'All Wins'        =>  '',
-		//Reference contains 3
-		'Reference'        =>  '4, 8, 9, 3, 6',
 		'favorite' =>  '4',
 		'WIN BETS' => [
-			2 =>  10,
-			4 =>  40,
-			6 =>  10,
-			7 =>  10,
-			8 =>  30,
+			1 =>  -10,
+			2 =>  -10,
+			3 =>  -10,
+			4 =>  -10,
+			6 =>  -10,
 		],
-		//Total bets:100',
-		//count:5',
+		//Total bets:-50',
+		//count:7',
 	],
 	'8' => [
 		/**
 		Race 8
 		*/
-		'wins' =>  [[4]] ,
-		'qpl/trio'       =>  [[3, 1, 5, 8, 4]] ,
 		'All Runners   '  =>  '3, 4, 1, 5, 7, 12, 10, 8, 9, 2, 11, 6',
-		'All QPL values'  =>  '3, 4, 1, 5, 8',
-		'All Wins'        =>  '4',
-		//Reference contains 3
-		'Reference'        =>  '3, 1, 5, 8',
 		'favorite' =>  '3',
 		'WIN BETS' => [
+			1 =>  40,
 			2 =>  10,
-			3 =>  30,
+			3 =>  80,
+			4 =>  70,
 			6 =>  10,
-			9 =>  10,
-			12 =>  10,
+			9 =>  20,
+			12 =>  20,
 		],
-		//Total bets:70',
-		//count:5',
+		//Total bets:250',
+		//count:7',
 	],
 	'9' => [
 		/**
 		Race 9
 		*/
-		'wins' =>  [[1]] ,
-		'qpl/trio'       =>  [[6, 11, 4, 9, 1]] ,
 		'All Runners   '  =>  '6, 11, 4, 9, 2, 1, 3, 5, 7, 10, 8, 12',
-		'All QPL values'  =>  '6, 11, 4, 9, 1',
-		'All Wins'        =>  '1',
-		'Reference'        =>  '6, 11, 4, 9',
 		'favorite' =>  '6',
 		'WIN BETS' => [
-			1 =>  10,
-			3 =>  10,
-			4 =>  20,
-			6 =>  50,
+			1 =>  20,
+			3 =>  20,
+			4 =>  40,
+			6 =>  90,
+			7 =>  10,
 			8 =>  10,
+			9 =>  30,
 		],
-		//Total bets:100',
-		//count:5',
+		//Total bets:220',
+		//count:7',
 	],
 ];
--- sortodds.php
+++ sortodds.php
@@ -1,32 +1,5 @@
 <?php
 
-function numericalValue($n){
-    $tens = intdiv($n, 10);
-    $units = $n - 10 * $tens;
-    return $tens + $units;
-}
-/**
- * Returns true if array $needle is contained in array of arrays $haystack 
- */
-function in_my_array($needle, $haystack){
-    foreach($haystack as $comparedTo){
-        $shit1 = array_values($needle);
-        $shit2 = array_values($comparedTo);
-        sort($shit1); 
-        sort($shit2);
-        $count1 = count($shit1);
-        $count2 = count($shit2);
-        if($count1 == $count2){
-            $coincidences = 0;
-            for($compt = 0; $compt < $count1; $compt++) {
-                if($shit1[$compt] == $shit2[$compt]) $coincidences++;
-            }
-            if($coincidences == $count1) return true;
-        }
-    }
-    return false;
-}
-
 function getWeights($odds, $profit = 0, $precision = 10){
     $weights = [];
     $totalWeights = 0;
@@ -69,7 +42,6 @@ $outFile = $currentDir . DIRECTORY_SEPARATOR . "$step.php";
 if(file_exists($outFile)){
     $oldData = include($outFile);
 }
-$probas = [];
 
 $reds = [1, 3, 5, 7, 9, 12, 14, 16, 18, 
          19, 21, 23, 25, 27, 30, 32, 34, 36];
@@ -79,43 +51,15 @@ $blacks = [2, 4, 6, 8, 2, 11, 13, 15, 17, 20,
 
 $totalRaces = count($allOdds);
 
-for($r=1; $r <= $totalRaces; $r++){
-    if(!isset($allOdds[$r])) continue;
-    $odds = $allOdds[$r];
-    $proba = [];
-    $sum = 0;
-    foreach($odds as $i => $oddsI){
-            $proba[$i] = 100 * (log($oddsI) / $oddsI) / exp(1);
-            $sum += $proba[$i];
-            }
-    foreach($odds as  $i => $oddsI){
-        //adjust to 100 percentage
-        $proba[$i] = round( $proba[$i] * 100 / $sum, 2);
-    }
-    arsort($proba);
-    $probas[$r] = $proba;
-}
-
-
 $outtext = "<?php\n\n";
 $outtext .= "return [\n";
 
 for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
-    if(!isset($probas[$raceNumber])) continue;
-
-    if(isset($oldData)){
-        if(isset($oldData[$raceNumber])){
-            $oldRaceData = $oldData[$raceNumber];
-            if(isset($oldRaceData['wins'])) $oldWINS = $oldRaceData['wins'];
-            if(isset($oldRaceData['qpl/trio'])) $oldQPLTrio = $oldRaceData['qpl/trio'];
-        }
-    }
+    if(!isset($allOdds[$raceNumber])) continue;
     $racetext = "";
-
-    $tmpArray = $probas[$raceNumber];
+    $tmpArray = $allOdds[$raceNumber];
+    asort($tmpArray);
     $runners = array_keys($tmpArray);
-
-    if(count($runners) < 10) continue;
     
     $racetext .= "\t'$raceNumber' => [\n";
     $racetext .= "\t\t/**\n";
@@ -138,88 +82,6 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
        $others =$sBlacks;
     }
 
-    $trio = array_merge(array_slice($favorites, 0, 3), array_slice($others, 0, 2));
-
-    $qplLeftSide = [$favorites[0], $favorites[1], $favorites[2], $others[0], $others[1]];
-    $qplRightSide = [ $others[3], $favorites[count($favorites) - 3], $others[count($others) - 3], end($favorites), end($others) ];
-    $toWin = [];
-    for($indexL = 0; $indexL < count($qplLeftSide); $indexL++) {
-        for($indexR = 0; $indexR < count($qplRightSide); $indexR++) {
-            $left = $qplLeftSide[$indexL];
-            $right = $qplRightSide[$indexR];
-            if( 
-                (abs(numericalValue($left) - numericalValue($right)) <= 2)
-                &&
-                (
-                    (in_array($left, $sReds) && in_array($right, $sBlacks))
-                    || (in_array($left, $sBlacks) && in_array($right, $sReds))
-                )
-            ){
-                if(!in_array($left, $toWin)) $toWin[] = $left;
-                if(!in_array($right, $toWin)) $toWin[] = $right;
-            }
-        }
-    }
-    $difference = array_values(array_unique(array_diff($trio, $toWin)));
-    $intersection = array_Values(array_unique(array_intersect($toWin, $trio)));
-
-    if(isset($oldWINS)) $wins = $oldWINS;
-    else $wins = [];
-
-    if(isset($oldQPLTrio)) $qplTrios = $oldQPLTrio;
-    else $qplTrios = [];
-    
-    if(!empty($difference)) {
-        if(!in_my_array($difference, $wins)) $wins[] = $difference;
-    } 
-
-    $qplTrio = array_values(array_unique(array_merge($intersection, $difference)));
-    if(!in_my_array($qplTrio, $qplTrios)) $qplTrios[] = $qplTrio;
-        
-    $WINSText = "[";
-    $allWinsValues = [];
-    $someCounter = 0;
-    $someLength = count($wins);
-    foreach($wins as $winsItem){
-        $allWinsValues = array_values(array_unique(array_merge($allWinsValues, $winsItem)));
-        $WINSText .= "[" . implode(", ", $winsItem) . "]";
-        $someCounter ++;
-        if($someCounter < $someLength) $WINSText .= ", ";
-    }
-    $WINSText .= "]";
-
-    $allQplValues = [];
-    $QPLText = "[";
-    $someCounter = 0;
-    $someLength = count($qplTrios);
-    foreach($qplTrios as $qplItem){
-        $allQplValues = array_values(array_unique(array_merge($allQplValues, $qplItem)));
-        $QPLText .= "[" . implode(", ", $qplItem) . "]";
-        $someCounter ++;
-        if($someCounter < $someLength) $QPLText .= ", ";
-    }
-    $QPLText .= "]";
-
-    //1. Sort  qplValues by odds
-    $qplsOdds = [];
-    foreach($allQplValues as $iIndex){
-        if(isset($allOdds[$raceNumber][$iIndex])) $qplsOdds[$iIndex] = $allOdds[$raceNumber][$iIndex];
-    }
-    asort($qplsOdds);
-    $allQplValues = array_keys($qplsOdds);
-
-    $showRace = true;
-
-    //Sort  allWinsValues by odds
-    $qplsOdds = [];
-    foreach($allWinsValues as $iKey => $iIndex){
-        if(isset($allOdds[$raceNumber][$iIndex])) $qplsOdds[$iIndex] = $allOdds[$raceNumber][$iIndex];
-        else $showRace = false;
-    }
-    asort($qplsOdds);
-    $allWinsValues = array_keys($qplsOdds);
-    $forReference = array_diff($allQplValues, $allWinsValues);
-
     //determine odds weights
     //1. gets first five candidates depening on favorite
     $favKeys = explode(", ", $favData[$first1]['fav']);
@@ -232,14 +94,7 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
     }
     $weights = getWeights($favOdds, 2, 10);
     
-    $racetext .= "\t\t'wins' =>  $WINSText ,\n";
-    $racetext .= "\t\t'qpl/trio'       =>  $QPLText ,\n";
     $racetext .= "\t\t'All Runners   '  =>  '" . implode(", ", $runners).  "',\n";
-    $racetext .= "\t\t'All QPL values'  =>  '" . implode(", ", $allQplValues).  "',\n";
-    if(in_array(3, $allWinsValues)) $racetext .= "\t\t//All wins contains 3\n";
-    $racetext .= "\t\t'All Wins'        =>  '" . implode(", ", $allWinsValues).  "',\n";
-    if(in_array(3, $forReference)) $racetext .= "\t\t//Reference contains 3\n";
-    $racetext .= "\t\t'Reference'        =>  '" . implode(", ", $forReference).  "',\n";
     $racetext .= "\t\t'favorite' =>  '" . $first1 . "',\n";
     $totalBets = 0;
     $racetext .= "\t\t'WIN BETS' => [\n";
@@ -254,9 +109,7 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
     $racetext .= "\t\t//Total bets:" . $totalBets . "',\n";
     $racetext .= "\t\t//count:" . count($weights) . "',\n";
     $racetext .= "\t],\n";
-    unset($oldWINS);
-    unset($oldQPLTrio);
-    if($showRace) $outtext .= $racetext;
+    $outtext .= $racetext;
 }
 
 $outtext .= "];\n";
