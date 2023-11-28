bff9b34665ee9c9a957a53ce3687f12c55e178f7
artistic software <aadel.iimam@gmail.com>
2023-11-28 04:51:59 +0000

    ...

---
+++ 1129/1.php
@@ -0,0 +1,82 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'All Runners   '  =>  '3, 6, 7, 2, 9, 8, 1, 5, 4',
+		'Total Runners '  =>  9,
+		'places' => '2, 9, 8, 1',
+		'places4' => '2, 9, 8, 1',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'All Runners   '  =>  '5, 4, 1, 2, 3, 6, 7, 8',
+		'Total Runners '  =>  8,
+		'WP' => '5',
+		'places' => '6, 7, 8',
+		'places4' => '6, 7, 8',
+		'candidate(k = 8)' => '5',
+		'candidate(k = 7)' => '4',
+		'candidate(k = 6)' => '1',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'All Runners   '  =>  '7, 1, 11, 5, 9, 2, 6, 3, 4, 8, 10',
+		'Total Runners '  =>  11,
+		'places' => '1, 11, 9, 5',
+		'places4' => '5, 9',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'All Runners   '  =>  '7, 10, 2, 6, 5, 1, 11, 8, 4, 3, 12, 9',
+		'Total Runners '  =>  12,
+		'places' => '4, 3, 12, 9',
+		'places4' => '4, 3, 12, 9',
+		'candidate(k = 8)' => '5',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'All Runners   '  =>  '3, 5, 1, 11, 2, 7, 9, 10, 8, 4, 6, 12',
+		'Total Runners '  =>  12,
+		'WP' => '3',
+		'places' => '6, 12',
+		'places4' => '6, 12',
+		'candidate(k = 12)' => '3',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'All Runners   '  =>  '5, 11, 1, 3, 7, 2, 9, 12, 4, 8, 6, 10',
+		'Total Runners '  =>  12,
+		'places' => '9, 12, 4',
+		'places4' => '9, 12, 4',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'All Runners   '  =>  '1, 7, 10, 5, 3, 4, 6, 9, 8, 2',
+		'Total Runners '  =>  10,
+		'places' => '7, 10, 5, 3',
+		'places4' => '5, 3',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'All Runners   '  =>  '2, 11, 6, 5, 8, 1, 9, 4, 7, 3, 12',
+		'Total Runners '  =>  11,
+		'places' => '2, 11, 6',
+	],
+];
