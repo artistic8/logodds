61408bafe69b6f3f9e9c69ea33deaf49831847c2
artistic software <aadel.iimam@gmail.com>
2023-11-28 08:24:53 +0000

    ...

--- 1129/1.php
+++ 1129/1.php
@@ -5,81 +5,78 @@ return [
 		/**
 		Race 1
 		*/
-		'All Runners   '  =>  '3, 7, 9, 6, 2, 8, 1, 5, 4',
+		'All Runners   '  =>  '3, 8, 9, 7, 6, 2, 1, 5, 4',
 		'Total Runners '  =>  9,
-		'places' => '7, 9, 6, 2, 8, 1',
-		'places4' => '6, 2, 8, 1',
+		'places' => '8, 9, 7, 6, 2, 1',
+		'places4' => '7, 6, 2, 1',
 	],
 	'2' => [
 		/**
 		Race 2
 		*/
-		'All Runners   '  =>  '5, 4, 1, 3, 2, 6, 7, 8',
+		'All Runners   '  =>  '5, 4, 3, 2, 1, 8, 6, 7',
 		'Total Runners '  =>  8,
-		'WP' => '5',
-		'places' => '6, 7, 8',
-		'places4' => '6, 7, 8',
-		'candidate(k = 8)' => '5',
-		'candidate(k = 7)' => '4',
-		'candidate(k = 6)' => '1',
+		'places' => '1, 8, 6, 7',
+		'places4' => '1, 8, 6, 7',
 	],
 	'3' => [
 		/**
 		Race 3
 		*/
-		'All Runners   '  =>  '7, 1, 11, 5, 9, 2, 6, 3, 4, 8, 10',
+		'All Runners   '  =>  '7, 11, 4, 5, 1, 3, 6, 9, 8, 2, 10',
 		'Total Runners '  =>  11,
-		'places' => '1, 11, 5, 9',
-		'places4' => '5, 9',
+		'places' => '7, 11, 4, 5, 1, 9',
+		'places4' => '5, 1, 9',
 	],
 	'4' => [
 		/**
 		Race 4
 		*/
-		'All Runners   '  =>  '7, 10, 2, 6, 5, 1, 8, 11, 4, 3, 12, 9',
+		'All Runners   '  =>  '7, 10, 1, 5, 6, 2, 4, 8, 11, 9, 3, 12',
 		'Total Runners '  =>  12,
-		'places' => '4, 3, 12, 9',
-		'places4' => '4, 3, 12, 9',
+		'WP' => '7',
+		'places' => '4, 9, 3, 12',
+		'places4' => '4, 9, 3, 12',
+		'candidate(k = 12)' => '7',
+		'candidate(k = 8)' => '6',
 	],
 	'5' => [
 		/**
 		Race 5
 		*/
-		'All Runners   '  =>  '5, 3, 1, 2, 11, 7, 9, 10, 4, 8, 6, 12',
+		'All Runners   '  =>  '1, 3, 5, 11, 2, 7, 9, 10, 4, 8, 12, 6',
 		'Total Runners '  =>  12,
-		'WP' => '5',
-		'places' => '6, 12',
-		'places4' => '6, 12',
-		'candidate(k = 12)' => '5',
+		'places' => '8, 12, 6',
+		'places4' => '8, 12, 6',
 	],
 	'6' => [
 		/**
 		Race 6
 		*/
-		'All Runners   '  =>  '5, 11, 1, 3, 7, 2, 9, 12, 6, 8, 4, 10',
+		'All Runners   '  =>  '5, 11, 1, 2, 3, 7, 8, 12, 9, 4, 10, 6',
 		'Total Runners '  =>  12,
-		'places' => '9, 12, 6, 4',
-		'places4' => '9, 12, 6, 4',
+		'places' => '8, 12, 9, 4, 6',
+		'places4' => '8, 12, 9, 4, 6',
+		'candidate(k = 9)' => '2',
 	],
 	'7' => [
 		/**
 		Race 7
 		*/
-		'All Runners   '  =>  '1, 7, 10, 3, 5, 6, 4, 8, 9, 2',
+		'All Runners   '  =>  '1, 7, 5, 10, 3, 4, 6, 8, 9, 2',
 		'Total Runners '  =>  10,
-		'places' => '7, 10, 3, 5',
-		'places4' => '3, 5',
+		'places' => '7, 5, 10, 3',
+		'places4' => '10, 3',
 		'candidate(k = 9)' => '7',
-		'candidate(k = 8)' => '10',
-		'candidate(k = 6)' => '5',
+		'candidate(k = 8)' => '5',
 	],
 	'8' => [
 		/**
 		Race 8
 		*/
-		'All Runners   '  =>  '2, 11, 5, 6, 8, 1, 4, 9, 7, 3, 12',
+		'All Runners   '  =>  '5, 2, 4, 6, 11, 8, 1, 9, 3, 7, 12',
 		'Total Runners '  =>  11,
-		'places' => '2, 11, 5, 6',
-		'places4' => '6',
+		'places' => '5, 2, 6, 11, 8',
+		'places4' => '6, 11, 8',
 	],
 ];
