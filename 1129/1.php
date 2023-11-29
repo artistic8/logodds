bf1abca7fc3968868958fd6e947e8ecbc0e10cd9
artistic software <aadel.iimam@gmail.com>
2023-11-29 11:32:46 +0000

    ...

--- 1129/1.php
+++ 1129/1.php
@@ -29,8 +29,8 @@ return [
 		/**
 		Race 3
 		*/
-		'Win Odds'  =>  '7, 5, 1, 11, 4, 3, 6, 9, 8, 10, 2',
-		'Pla Odds'  =>  '7, 5, 11, 1, 3, 4, 6, 8, 9, 10, 2',
+		'Win Odds'  =>  '7, 5, 11, 1, 4, 3, 6, 9, 8, 10, 2',
+		'Pla Odds'  =>  '7, 5, 11, 1, 3, 6, 4, 8, 9, 10, 2',
 		'Total Runners '  =>  11,
 		//Based on win odds
 		'candidate(k = 10)' => '5',
@@ -38,6 +38,7 @@ return [
 		'candidate(k = 10)' => '5',
 		'candidate(k = 9)' => '11',
 		'candidate(k = 8)' => '1',
+		'candidate(k = 6)' => '6',
 	],
 	'4' => [
 		/**
@@ -84,11 +85,11 @@ return [
 		Race 7
 		*/
 		'Win Odds'  =>  '1, 10, 7, 5, 3, 4, 8, 2, 6, 9',
-		'Pla Odds'  =>  '5, 10, 1, 7, 4, 8, 3, 2, 9, 6',
+		'Pla Odds'  =>  '1, 5, 10, 7, 4, 8, 3, 2, 9, 6',
 		'Total Runners '  =>  10,
 		//Based on win odds
 		//Based on pla odds
-		'candidate(k = 9)' => '10',
+		'candidate(k = 9)' => '5',
 	],
 	'8' => [
 		/**
