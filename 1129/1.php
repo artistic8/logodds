0963f260593e190f74bf65f728cbb2d902d3e941
artistic software <aadel.iimam@gmail.com>
2023-11-29 12:46:07 +0000

    ...

--- 1129/1.php
+++ 1129/1.php
@@ -46,8 +46,8 @@ return [
 		/**
 		Race 4
 		*/
-		'Win Odds'  =>  '7, 2, 10, 6, 8, 5, 1, 4, 11, 12, 3, 9',
-		'Pla Odds'  =>  '7, 6, 2, 10, 8, 1, 5, 11, 4, 12, 3, 9',
+		'Win Odds'  =>  '7, 5, 2, 10, 6, 1, 8, 12, 4, 11, 3, 9',
+		'Pla Odds'  =>  '7, 5, 2, 10, 6, 1, 8, 12, 11, 4, 3, 9',
 		'Total Runners '  =>  12,
 		//Based on win odds
 		//Based on pla odds
@@ -57,7 +57,7 @@ return [
 		Race 5
 		*/
 		'Win Odds'  =>  '5, 3, 1, 11, 2, 7, 9, 10, 8, 4, 6, 12',
-		'Pla Odds'  =>  '5, 3, 1, 11, 2, 9, 7, 8, 10, 4, 6, 12',
+		'Pla Odds'  =>  '5, 1, 3, 11, 2, 9, 7, 8, 10, 4, 6, 12',
 		'Total Runners '  =>  12,
 		//Based on win odds
 		'WP' => '5',
@@ -72,35 +72,32 @@ return [
 		/**
 		Race 6
 		*/
-		'Win Odds'  =>  '5, 2, 3, 11, 1, 7, 4, 8, 12, 9, 10, 6',
-		'Pla Odds'  =>  '5, 2, 11, 3, 4, 1, 7, 9, 10, 12, 8, 6',
+		'Win Odds'  =>  '5, 2, 11, 3, 1, 7, 4, 8, 12, 9, 10, 6',
+		'Pla Odds'  =>  '5, 2, 11, 3, 1, 4, 7, 9, 10, 12, 8, 6',
 		'Total Runners '  =>  12,
 		//Based on win odds
 		'candidate(k = 8)' => '1',
 		//Based on pla odds
-		'candidate(k = 7)' => '1',
+		'candidate(k = 7)' => '4',
 	],
 	'7' => [
 		/**
 		Race 7
 		*/
-		'Win Odds'  =>  '1, 10, 7, 5, 3, 4, 8, 2, 9, 6',
-		'Pla Odds'  =>  '1, 5, 10, 7, 4, 3, 9, 8, 2, 6',
+		'Win Odds'  =>  '1, 10, 7, 5, 3, 4, 8, 2, 6, 9',
+		'Pla Odds'  =>  '1, 5, 10, 7, 4, 3, 8, 9, 2, 6',
 		'Total Runners '  =>  10,
 		//Based on win odds
-		'candidate(k = 9)' => '10',
 		//Based on pla odds
-		'candidate(k = 8)' => '10',
 	],
 	'8' => [
 		/**
 		Race 8
 		*/
-		'Win Odds'  =>  '5, 2, 4, 8, 6, 11, 1, 9, 7, 3, 12',
-		'Pla Odds'  =>  '5, 8, 4, 2, 11, 6, 9, 1, 7, 3, 12',
+		'Win Odds'  =>  '4, 5, 2, 8, 6, 11, 1, 9, 7, 3, 12',
+		'Pla Odds'  =>  '5, 8, 4, 2, 11, 9, 6, 1, 7, 3, 12',
 		'Total Runners '  =>  11,
 		//Based on win odds
 		//Based on pla odds
-		'candidate(k = 6)' => '6',
 	],
 ];
