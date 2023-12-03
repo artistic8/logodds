f9b43914b3f23f1235415a5dd2bfe4f78674a9cc
artistic software <aadel.iimam@gmail.com>
2023-12-03 06:48:15 +0000

    ...

--- 1203/1.php
+++ 1203/1.php
@@ -69,7 +69,7 @@ return [
 		Race 5
 		*/
 		'Win Odds'  =>  '2, 7, 6, 4, 3, 1, 8, 11, 12, 10, 5, 9',
-		'Pla Odds'  =>  '7, 2, 6, 4, 3, 1, 12, 8, 11, 10, 5, 9',
+		'Pla Odds'  =>  '2, 7, 4, 6, 1, 3, 12, 8, 11, 10, 5, 9',
 		'Total Runners '  =>  12,
 		/** Based on win odds */
 		'win odds candidate(k = 10)' => '6',
@@ -77,8 +77,8 @@ return [
 		'black win odds candidates' => '4, 6',
 		'SURE PLACE' => '6',
 		/** Based on pla odds */
-		'pla odds candidate(k = 10)' => '6',
-		'pla odds candidate(k = 8)' => '3',
+		'pla odds candidate(k = 10)' => '4',
+		'pla odds candidate(k = 8)' => '1',
 		'black pla odds candidates' => '2, 4, 6',
 		'SURE PLACE' => '2, 6',
 	],
@@ -87,7 +87,7 @@ return [
 		Race 6
 		*/
 		'Win Odds'  =>  '7, 4, 3, 5, 2, 1, 6, 8, 10, 9',
-		'Pla Odds'  =>  '7, 4, 3, 5, 1, 2, 6, 10, 8, 9',
+		'Pla Odds'  =>  '7, 3, 4, 5, 1, 2, 6, 10, 8, 9',
 		'Total Runners '  =>  10,
 		/** Based on win odds */
 		'win odds candidate(k = 8)' => '3',
@@ -100,14 +100,14 @@ return [
 		/**
 		Race 7
 		*/
-		'Win Odds'  =>  '4, 2, 6, 1, 5, 3, 11, 7, 8, 10, 9',
+		'Win Odds'  =>  '4, 2, 1, 6, 5, 3, 11, 7, 8, 10, 9',
 		'Pla Odds'  =>  '2, 1, 4, 6, 5, 3, 11, 7, 8, 10, 9',
 		'Total Runners '  =>  11,
 		/** Based on win odds */
 		'win odds candidate(k = 10)' => '2',
 		/** Black selection */
 		'black win odds candidates' => '6, 2',
-		'SURE PLACE' => '6, 2',
+		'SURE PLACE' => '2',
 		/** Based on pla odds */
 		'pla odds candidate(k = 10)' => '1',
 		'black pla odds candidates' => '2, 4',
@@ -117,8 +117,8 @@ return [
 		/**
 		Race 8
 		*/
-		'Win Odds'  =>  '9, 5, 11, 8, 1, 2, 4, 13, 6, 3, 7, 12, 10',
-		'Pla Odds'  =>  '1, 11, 9, 5, 4, 13, 8, 2, 3, 6, 7, 10, 12',
+		'Win Odds'  =>  '9, 5, 11, 8, 1, 2, 4, 13, 6, 7, 3, 12, 10',
+		'Pla Odds'  =>  '1, 11, 9, 5, 4, 13, 8, 2, 6, 3, 7, 10, 12',
 		'Total Runners '  =>  13,
 		/** Based on win odds */
 		'win odds candidate(k = 12)' => '5',
@@ -134,23 +134,23 @@ return [
 		Race 9
 		*/
 		'Win Odds'  =>  '6, 2, 1, 10, 3, 4, 13, 9, 12, 5, 8, 14, 7, 11',
-		'Pla Odds'  =>  '2, 6, 10, 1, 3, 13, 12, 4, 8, 9, 14, 5, 7, 11',
+		'Pla Odds'  =>  '2, 6, 10, 1, 13, 3, 12, 4, 9, 8, 14, 5, 7, 11',
 		'Total Runners '  =>  14,
 		/** Based on win odds */
 		//No win candidates(Place bet: 6)
 		/** Based on pla odds */
-		//No pla candidates(Place bet: 2 <== focus on this one for now)
+		'pla odds candidate(k = 9)' => '3',
 		'black pla odds candidates' => '10, 4',
 	],
 	'10' => [
 		/**
 		Race 10
 		*/
-		'Win Odds'  =>  '4, 5, 8, 3, 11, 12, 6, 1, 2, 10, 13, 7, 14, 9',
+		'Win Odds'  =>  '4, 5, 8, 3, 12, 11, 6, 1, 2, 10, 13, 7, 14, 9',
 		'Pla Odds'  =>  '4, 5, 12, 8, 11, 6, 3, 1, 2, 10, 13, 7, 14, 9',
 		'Total Runners '  =>  14,
 		/** Based on win odds */
-		'win odds candidate(k = 10)' => '11',
+		'win odds candidate(k = 10)' => '12',
 		/** Black selection */
 		'black win odds candidates' => '8, 4, 11',
 		'SURE PLACE' => '8, 4',
