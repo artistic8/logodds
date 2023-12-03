d4b69b55cd5cd8ba104348edf9ec0482adce857d
artistic software <aadel.iimam@gmail.com>
2023-12-03 07:58:24 +0000

    ...

--- 1203/1.php
+++ 1203/1.php
@@ -102,7 +102,7 @@ return [
 		Race 7
 		*/
 		'Win Odds'  =>  '4, 2, 1, 5, 6, 3, 11, 7, 10, 8, 9',
-		'Pla Odds'  =>  '2, 4, 1, 6, 5, 3, 11, 7, 8, 10, 9',
+		'Pla Odds'  =>  '2, 4, 1, 5, 6, 3, 11, 7, 10, 8, 9',
 		'Total Runners '  =>  11,
 		/** Based on win odds */
 		//No win candidates(Place bet: 4)
@@ -110,7 +110,7 @@ return [
 		'black win odds candidates' => '6, 2',
 		'SURE PLACE' => '2',
 		/** Based on pla odds */
-		'pla odds candidate(k = 10)' => '4',
+		//No pla candidates(Place bet: 2 <== focus on this one for now)
 		'black pla odds candidates' => '2, 4',
 		'SURE PLACE' => '2, 4',
 	],
@@ -119,14 +119,14 @@ return [
 		Race 8
 		*/
 		'Win Odds'  =>  '9, 5, 11, 8, 1, 2, 4, 13, 6, 7, 3, 12, 10',
-		'Pla Odds'  =>  '11, 9, 1, 5, 4, 13, 2, 8, 6, 3, 7, 12, 10',
+		'Pla Odds'  =>  '9, 11, 1, 5, 4, 13, 2, 8, 6, 3, 7, 12, 10',
 		'Total Runners '  =>  13,
 		/** Based on win odds */
 		'win odds candidate(k = 12)' => '5',
 		/** Black selection */
 		'black win odds candidates' => '8',
 		/** Based on pla odds */
-		'pla odds candidate(k = 12)' => '9',
+		'pla odds candidate(k = 12)' => '11',
 		'pla odds candidate(k = 8)' => '13',
 		'black pla odds candidates' => '2, 11, 13',
 		'SURE PLACE' => '11',
@@ -136,14 +136,14 @@ return [
 		Race 9
 		*/
 		'Win Odds'  =>  '6, 2, 1, 10, 3, 4, 13, 5, 9, 12, 8, 14, 7, 11',
-		'Pla Odds'  =>  '2, 6, 10, 1, 13, 3, 12, 4, 5, 9, 8, 14, 7, 11',
+		'Pla Odds'  =>  '2, 6, 10, 1, 3, 13, 4, 12, 9, 5, 8, 14, 7, 11',
 		'Total Runners '  =>  14,
 		/** Based on win odds */
 		'win odds candidate(k = 9)' => '4',
 		/** Black selection */
 		'black win odds candidates' => '4, 13',
 		/** Based on pla odds */
-		//No pla candidates(Place bet: 2 <== focus on this one for now)
+		'pla odds candidate(k = 9)' => '13',
 		'black pla odds candidates' => '10, 4, 13',
 	],
 	'10' => [
@@ -151,7 +151,7 @@ return [
 		Race 10
 		*/
 		'Win Odds'  =>  '4, 5, 8, 3, 11, 12, 6, 1, 2, 10, 13, 7, 9, 14',
-		'Pla Odds'  =>  '4, 5, 12, 8, 11, 3, 6, 1, 2, 13, 10, 14, 7, 9',
+		'Pla Odds'  =>  '4, 5, 12, 8, 11, 3, 6, 1, 13, 2, 10, 14, 7, 9',
 		'Total Runners '  =>  14,
 		/** Based on win odds */
 		'WP' => '4',
