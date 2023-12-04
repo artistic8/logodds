31d6e6da4ca55778403111a8ddb891ed62dac2a2
artistic software <aadel.iimam@gmail.com>
2023-12-04 07:19:31 +0000

    bug: previous commits returning same data

--- 1203/history.php
+++ 1203/history.php
@@ -5,154 +5,60 @@ return [
 		/**
 		Race 1
 		*/
-		'Win Odds'  =>  '11, 1, 4, 13, 3, 7, 8, 10, 2, 12, 6, 14, 9, 5',
-		'Pla Odds'  =>  '11, 1, 4, 13, 7, 3, 8, 10, 2, 12, 6, 14, 9, 5',
-		'Total Runners '  =>  14,
-		/** Based on win odds */
-		//No win candidates(Place bet: 11)
-		/** Black selection */
-		'black win odds candidates' => '11, 10',
-		'SURE PLACE' => '11',
-		/** Based on pla odds */
-		//No pla candidates(Place bet: 11 <== focus on this one for now)
-		'black pla odds candidates' => '10',
+		'Runners by odds mvnt'  =>  '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
 	],
 	'2' => [
 		/**
 		Race 2
 		*/
-		'Win Odds'  =>  '9, 7, 1, 6, 8, 5, 3, 10, 12, 4, 2, 11',
-		'Pla Odds'  =>  '9, 7, 6, 8, 1, 5, 3, 10, 12, 4, 2, 11',
-		'Total Runners '  =>  12,
-		/** Based on win odds */
-		//No win candidates(Place bet: 9)
-		/** Based on pla odds */
-		//No pla candidates(Place bet: 9 <== focus on this one for now)
-		'black pla odds candidates' => '10',
+		'Runners by odds mvnt'  =>  '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
 	],
 	'3' => [
 		/**
 		Race 3
 		*/
-		'Win Odds'  =>  '2, 1, 7, 5, 10, 8, 14, 13, 3, 12, 9, 4, 6, 11',
-		'Pla Odds'  =>  '2, 5, 14, 8, 1, 7, 10, 13, 3, 12, 9, 4, 6, 11',
-		'Total Runners '  =>  14,
-		/** Based on win odds */
-		//No win candidates(Place bet: 2)
-		/** Black selection */
-		'black win odds candidates' => '2',
-		'SURE PLACE' => '2',
-		/** Based on pla odds */
-		//No pla candidates(Place bet: 2 <== focus on this one for now)
-		'black pla odds candidates' => '8, 10',
+		'Runners by odds mvnt'  =>  '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
 	],
 	'4' => [
 		/**
 		Race 4
 		*/
-		'Win Odds'  =>  '6, 12, 1, 5, 10, 7, 3, 4, 2, 11, 8, 9',
-		'Pla Odds'  =>  '1, 12, 6, 5, 7, 2, 10, 3, 4, 11, 8, 9',
-		'Total Runners '  =>  12,
-		/** Based on win odds */
-		//No win candidates(Place bet: 6)
-		/** Black selection */
-		'black win odds candidates' => '4',
-		/** Based on pla odds */
-		//No pla candidates(Place bet: 1 <== focus on this one for now)
-		'black pla odds candidates' => '6, 10',
-		'SURE PLACE' => '6',
+		'Runners by odds mvnt'  =>  '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
 	],
 	'5' => [
 		/**
 		Race 5
 		*/
-		'Win Odds'  =>  '2, 4, 7, 6, 1, 3, 8, 11, 12, 10, 5, 9',
-		'Pla Odds'  =>  '2, 7, 4, 6, 1, 3, 8, 11, 12, 10, 9, 5',
-		'Total Runners '  =>  12,
-		/** Based on win odds */
-		'win odds candidate(k = 10)' => '7',
-		/** Based on pla odds */
-		'pla odds candidate(k = 10)' => '4',
-		'black pla odds candidates' => '2, 4',
-		'SURE PLACE' => '2, 4',
+		'Runners by odds mvnt'  =>  '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
 	],
 	'6' => [
 		/**
 		Race 6
 		*/
-		'Win Odds'  =>  '7, 4, 3, 5, 2, 1, 6, 8, 10, 9',
-		'Pla Odds'  =>  '7, 4, 3, 5, 2, 1, 6, 8, 10, 9',
-		'Total Runners '  =>  10,
-		/** Based on win odds */
-		'win odds candidate(k = 8)' => '3',
-		/** Based on pla odds */
-		'pla odds candidate(k = 8)' => '3',
-		'black pla odds candidates' => '4, 10',
-		'SURE PLACE' => '4',
+		'Runners by odds mvnt'  =>  '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
 	],
 	'7' => [
 		/**
 		Race 7
 		*/
-		'Win Odds'  =>  '4, 2, 6, 1, 5, 3, 11, 7, 8, 10, 9',
-		'Pla Odds'  =>  '2, 4, 6, 1, 5, 3, 7, 11, 8, 10, 9',
-		'Total Runners '  =>  11,
-		/** Based on win odds */
-		'win odds candidate(k = 10)' => '2',
-		/** Black selection */
-		'black win odds candidates' => '6, 2',
-		'SURE PLACE' => '6, 2',
-		/** Based on pla odds */
-		'pla odds candidate(k = 10)' => '4',
-		'pla odds candidate(k = 7)' => '5',
-		'black pla odds candidates' => '2, 4',
-		'SURE PLACE' => '2, 4',
+		'Runners by odds mvnt'  =>  '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11',
 	],
 	'8' => [
 		/**
 		Race 8
 		*/
-		'Win Odds'  =>  '9, 5, 11, 8, 1, 2, 4, 13, 6, 3, 7, 12, 10',
-		'Pla Odds'  =>  '1, 5, 11, 9, 13, 4, 8, 2, 3, 6, 7, 12, 10',
-		'Total Runners '  =>  13,
-		/** Based on win odds */
-		'win odds candidate(k = 12)' => '5',
-		/** Black selection */
-		'black win odds candidates' => '8',
-		/** Based on pla odds */
-		'pla odds candidate(k = 12)' => '5',
-		'black pla odds candidates' => '2',
+		'Runners by odds mvnt'  =>  '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13',
 	],
 	'9' => [
 		/**
 		Race 9
 		*/
-		'Win Odds'  =>  '6, 2, 1, 10, 3, 4, 13, 9, 12, 8, 5, 14, 7, 11',
-		'Pla Odds'  =>  '2, 6, 10, 1, 3, 12, 13, 4, 8, 9, 14, 5, 7, 11',
-		'Total Runners '  =>  14,
-		/** Based on win odds */
-		//No win candidates(Place bet: 6)
-		/** Based on pla odds */
-		//No pla candidates(Place bet: 2 <== focus on this one for now)
-		'black pla odds candidates' => '10, 4',
+		'Runners by odds mvnt'  =>  '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
 	],
 	'10' => [
 		/**
 		Race 10
 		*/
-		'Win Odds'  =>  '4, 5, 8, 11, 12, 3, 6, 1, 2, 10, 13, 7, 14, 9',
-		'Pla Odds'  =>  '4, 5, 12, 8, 11, 6, 1, 2, 3, 10, 13, 7, 9, 14',
-		'Total Runners '  =>  14,
-		/** Based on win odds */
-		'win odds candidate(k = 10)' => '12',
-		/** Black selection */
-		'black win odds candidates' => '8, 4',
-		'SURE PLACE' => '8, 4',
-		/** Based on pla odds */
-		'WP' => '4',
-		'pla odds candidate(k = 14)' => '4',
-		'pla odds candidate(k = 10)' => '11',
-		'black pla odds candidates' => '4, 8, 11',
-		'SURE PLACE' => '4, 8',
+		'Runners by odds mvnt'  =>  '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
 	],
 ];
--- tracemvnt.php
+++ tracemvnt.php
@@ -14,7 +14,7 @@ if(file_exists($plaOddsFile)){
 $winPositionDifferences = [];
 foreach($allWinOdds as $raceNumber => $runners){
     $winPositionDifferences[$raceNumber] = [];
-    foreach($runners as $runner){
+    foreach($runners as $runner => $omg){
         $winPositionDifferences[$raceNumber][$runner] = 0;
     }
 }
@@ -32,15 +32,26 @@ foreach($output as $line){
 //limit search to last 50 commits
 $history = array_slice($history, 0, 50);
 exec("git config --global advice.detachedHead false");
-for($count = count($history); $count > 1; $count --){
-    $oldVersion = $history[$count - 1];
-    $newVersion = $history[$count - 2];
+// for($count = count($history); $count > 1; $count --){
+    // $oldVersion = $history[$count - 1];
+    $oldVersion = "30217ffa664eabedbcf626fc5133bd071d65fd00";
+    // $newVersion = $history[$count - 2];
+    $newVersion = "76936eef584ca934e2e9241e68d8465bf54ffcfc";
     exec("git checkout $oldVersion");
     $oldContents = include("$currentDir/1.php");
     exec("git checkout master");
     exec("git checkout $newVersion");
     $newContents = include("$currentDir/1.php");
     exec("git checkout master");
+    var_dump($oldContents[1]['Win Odds']);
+    var_dump($newContents[1]['Win Odds']); die();
+    $oldOdds = explode(", ", $oldContents[$raceNumber]['Win Odds']);
+    $oldRunnerPosition = array_search(11, $oldOdds);
+    var_dump($oldRunnerPosition);
+    $newOdds = explode(", ", $newContents[$raceNumber]['Win Odds']);
+    $newRunnerPosition = array_search(11, $newOdds);
+    var_dump($newRunnerPosition); die();
+    $winOddsPositionDiff = $newRunnerPosition - $oldRunnerPosition;
     foreach($allWinOdds as $raceNumber => $runners){
         foreach($runners as $runner => $whatever){
             $oldOdds = explode(", ", $oldContents[$raceNumber]['Win Odds']);
@@ -51,127 +62,26 @@ for($count = count($history); $count > 1; $count --){
             $winPositionDifferences[$raceNumber][$runner] += $winOddsPositionDiff;
         }
     }
-   
-}
-
-var_dump($winPositionDifferences); die();
-
-die();
-if(file_exists($outFile)){
-    $oldData = include($outFile);
-}
-
-$totalRaces = count($allWinOdds);
-
+// }
+var_dump($winPositionDifferences[1]); die();
 $outtext = "<?php\n\n";
 $outtext .= "return [\n";
-
+$totalRaces= count($allWinOdds);
 for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
     if(!isset($allWinOdds[$raceNumber])) continue;
 
-    if(isset($oldData)){
-        if(isset($oldData[$raceNumber])){
-            $oldRaceData = $oldData[$raceNumber];
-            if(isset($oldRaceData['history w'])) $historyWs = $oldRaceData['history w'];
-            if(isset($oldRaceData['history p'])) $historyPs = $oldRaceData['history p'];
-        }
-    }
-
-    if(isset($historyWs)) $historyW = explode(", ", $historyWs);
-    else $historyW = [];
-
-    if(isset($historyPs)) $historyP = explode(", ", $historyPs);
-    else $historyP = [];
-
     $racetext = "";
-    $winsArray = $allWinOdds[$raceNumber];
-    asort($winsArray);
-    $runners = array_keys($winsArray);
-    if(isset($allPlaOdds)){
-        $plaArray = $allPlaOdds[$raceNumber];
-        asort($plaArray);
-        $placers = array_keys($plaArray);
-    }
-    
     $racetext .= "\t'$raceNumber' => [\n";
     $racetext .= "\t\t/**\n";
     $racetext .= "\t\tRace $raceNumber\n";
     $racetext .= "\t\t*/\n";
 
-    $racetext .= "\t\t'Win Odds'  =>  '" . implode(", ", $runners).  "',\n";
-    if(isset($placers)){
-        $racetext .= "\t\t'Pla Odds'  =>  '" . implode(", ", $placers).  "',\n";
-    }
-    $racetext .= "\t\t'Total Runners '  =>  " . count($runners).  ",\n";
+    $runnersPositions = $winPositionDifferences[$raceNumber];
+    arsort($runnersPositions);
+    $runners = array_keys($runnersPositions);
 
-    $size = count($runners);
-    $racetext .= "\t\t/** Based on win odds */\n";
-    $first = $runners[0];
-    $first3 = array_slice($runners, 0, 3);
-    $pos = array_search($size, $runners);
-    for($k = $size; $k > floor($size / 2); $k --)
-    {
-        $historyWIsNew = true;
-        $posK = array_search($k, $runners);
-        if($posK + 1 == $k && isset($runners[$size - 1 - $posK])){
-           $candidate = $runners[$size - 1 - $posK];
-           if($historyWIsNew && in_array($candidate, $blacks) && !in_array($candidate, $historyW)){
-                $historyW[] = $candidate;
-                $historyWIsNew = false;
-           }
-           $racetext .= "\t\t'win odds candidate(". "k = $k)" . "' => '" . $candidate .  "',\n";
-        }
-    }
-    //1. Sort  historyW by odds
-    $qplsOdds = [];
-    foreach($historyW as $iIndex){
-        if(isset($allWinOdds[$raceNumber][$iIndex])) $qplsOdds[$iIndex] = $allWinOdds[$raceNumber][$iIndex];
-    }
-    asort($qplsOdds);
-    $historyW = array_keys($qplsOdds);
-    if(!empty($historyW)){
-        $racetext .= "\t\t/** Black selection */\n";
-        $racetext .= "\t\t'history w' => '" . implode(", ", $historyW).  "',\n";
-        $sureWinPlace = array_intersect($historyW, $first3);
-        if(!empty($sureWinPlace)){
-            $racetext .= "\t\t'SURE PLACE' => '" . implode(", ", $sureWinPlace).  "',\n";
-        }
-    }
-    if(isset($placers)){
-        $racetext .= "\t\t/** Based on pla odds */\n";
-        $first = $placers[0];
-        $pos = array_search($size, $placers);
-        for($k = $size; $k > floor($size / 2); $k --)
-        {
-            $posK = array_search($k, $placers);
-            if($posK + 1 == $k && isset($placers[$size - 1 - $posK])){
-                $candidate = $placers[$size - 1 - $posK];
-                if(in_array($candidate, $blacks) && !in_array($candidate, $historyP)){
-                        $historyP[] = $candidate;
-                }
-                $racetext .= "\t\t'pla odds candidate(". "k = $k)" . "' => '" . $candidate .  "',\n";
-            }
-        }
-        //1. Sort  historyP by odds
-        $qplsOdds = [];
-        foreach($historyP as $iIndex){
-            if(isset($allPlaOdds[$raceNumber][$iIndex])) $qplsOdds[$iIndex] = $allPlaOdds[$raceNumber][$iIndex];
-        }
-        asort($qplsOdds);
-        $historyP = array_keys($qplsOdds);
-        if(!empty($historyP)){
-            $racetext .= "\t\t'history p' => '" . implode(", ", $historyP).  "',\n";
-            $sureWinPlace = array_intersect($historyP, $first3);
-            if(!empty($sureWinPlace)){
-                $racetext .= "\t\t'SURE PLACE' => '" . implode(", ", $sureWinPlace).  "',\n";
-            }
-        }
-    }
+    $racetext .= "\t\t'Runners by odds mvnt'  =>  '" . implode(", ", $runners).  "',\n";
     $racetext .= "\t],\n";
-    unset($historyPs);
-    unset($historyP);
-    unset($historyWs);
-    unset($historyW);
     $outtext .= $racetext;
 }
 
