4f149d03a1baf1a6c9a33316154750d9b5a7d33e
artistic software <aadel.iimam@gmail.com>
2023-12-04 08:19:30 +0000

    ...

--- 1203/history.php
+++ 1203/history.php
@@ -5,60 +5,60 @@ return [
 		/**
 		Race 1
 		*/
-		'Runners by odds mvnt'  =>  '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'Runners by odds mvnt'  =>  '11, 1, 4, 13, 3, 7, 8, 10, 2, 12, 6, 14, 9, 5',
 	],
 	'2' => [
 		/**
 		Race 2
 		*/
-		'Runners by odds mvnt'  =>  '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'Runners by odds mvnt'  =>  '9, 7, 5, 1, 8, 6, 3, 10, 12, 2, 4, 11',
 	],
 	'3' => [
 		/**
 		Race 3
 		*/
-		'Runners by odds mvnt'  =>  '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'Runners by odds mvnt'  =>  '5, 10, 2, 1, 7, 14, 8, 12, 13, 6, 3, 4, 9, 11',
 	],
 	'4' => [
 		/**
 		Race 4
 		*/
-		'Runners by odds mvnt'  =>  '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'Runners by odds mvnt'  =>  '6, 1, 12, 5, 10, 3, 7, 2, 4, 8, 11, 9',
 	],
 	'5' => [
 		/**
 		Race 5
 		*/
-		'Runners by odds mvnt'  =>  '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'Runners by odds mvnt'  =>  '2, 6, 4, 7, 1, 12, 3, 8, 11, 10, 5, 9',
 	],
 	'6' => [
 		/**
 		Race 6
 		*/
-		'Runners by odds mvnt'  =>  '8, 2, 10, 3, 4, 7, 9, 1, 5, 6',
+		'Runners by odds mvnt'  =>  '7, 4, 3, 2, 5, 8, 1, 10, 6, 9',
 	],
 	'7' => [
 		/**
 		Race 7
 		*/
-		'Runners by odds mvnt'  =>  '3, 6, 10, 4, 7, 9, 11, 2, 5, 8, 1',
+		'Runners by odds mvnt'  =>  '4, 6, 2, 3, 1, 5, 11, 7, 10, 8, 9',
 	],
 	'8' => [
 		/**
 		Race 8
 		*/
-		'Runners by odds mvnt'  =>  '7, 13, 6, 3, 5, 9, 10, 12, 1, 2, 4, 8, 11',
+		'Runners by odds mvnt'  =>  '9, 5, 13, 7, 6, 11, 8, 1, 2, 4, 3, 12, 10',
 	],
 	'9' => [
 		/**
 		Race 9
 		*/
-		'Runners by odds mvnt'  =>  '9, 1, 5, 8, 14, 4, 6, 7, 10, 11, 2, 13, 3, 12',
+		'Runners by odds mvnt'  =>  '6, 1, 2, 10, 9, 4, 3, 13, 5, 8, 14, 12, 7, 11',
 	],
 	'10' => [
 		/**
 		Race 10
 		*/
-		'Runners by odds mvnt'  =>  '2, 9, 11, 12, 4, 13, 14, 1, 6, 7, 5, 8, 10, 3',
+		'Runners by odds mvnt'  =>  '4, 11, 12, 5, 8, 2, 3, 6, 1, 9, 13, 10, 7, 14',
 	],
 ];
