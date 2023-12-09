14c769f83f99b17dcdbea347b793a6f6d2e73de3
artistic software <aadel.iimam@gmail.com>
2023-12-09 02:31:22 +0000

    ...

--- 1203/history.php
+++ 1203/history.php
@@ -5,60 +5,90 @@ return [
 		/**
 		Race 1
 		*/
-		'Runners by odds mvnt'  =>  '8, 2, 11, 9, 1, 3, 7, 12, 13, 5, 10, 4, 6, 14',
+		'win odds mvnt'  =>  '8, 2, 11, 9, 1, 3, 7, 12, 13, 5, 10, 4, 6, 14, ',
+		'pla odds mvnt'  =>  '8, 7, 11, 1, 2, 12, 9, 3, 4, 5, 10, 13, 14, 6, ',
+		'Win suggestions'  =>  '11',
+		'Pla suggestions'  =>  '11, 1',
 	],
 	'2' => [
 		/**
 		Race 2
 		*/
-		'Runners by odds mvnt'  =>  '8, 12, 1, 5, 7, 4, 9, 10, 2, 6, 11, 3',
+		'win odds mvnt'  =>  '8, 12, 1, 5, 7, 4, 9, 10, 2, 6, 11, 3, ',
+		'pla odds mvnt'  =>  '5, 7, 8, 12, 9, 11, 1, 4, 2, 6, 10, 3, ',
+		'Win suggestions'  =>  '7, 5',
+		'Pla suggestions'  =>  '9, 7, 5',
 	],
 	'3' => [
 		/**
 		Race 3
 		*/
-		'Runners by odds mvnt'  =>  '6, 13, 10, 5, 8, 12, 2, 4, 14, 1, 3, 7, 11, 9',
+		'win odds mvnt'  =>  '6, 13, 10, 5, 8, 12, 2, 4, 14, 1, 3, 7, 11, 9, ',
+		'pla odds mvnt'  =>  '5, 12, 6, 13, 1, 4, 10, 2, 14, 3, 7, 8, 9, 11, ',
+		'Win suggestions'  =>  '5, 10, 2',
+		'Pla suggestions'  =>  '5, 1',
 	],
 	'4' => [
 		/**
 		Race 4
 		*/
-		'Runners by odds mvnt'  =>  '7, 5, 8, 2, 3, 1, 6, 12, 9, 10, 11, 4',
+		'win odds mvnt'  =>  '7, 5, 8, 2, 3, 1, 6, 12, 9, 10, 11, 4, ',
+		'pla odds mvnt'  =>  '2, 7, 8, 6, 11, 1, 5, 10, 12, 3, 4, 9, ',
+		'Win suggestions'  =>  '',
+		'Pla suggestions'  =>  '6',
 	],
 	'5' => [
 		/**
 		Race 5
 		*/
-		'Runners by odds mvnt'  =>  '11, 1, 12, 6, 2, 7, 4, 5, 9, 10, 3, 8',
+		'win odds mvnt'  =>  '11, 1, 12, 6, 2, 7, 4, 5, 9, 10, 3, 8, ',
+		'pla odds mvnt'  =>  '1, 12, 11, 2, 4, 6, 7, 8, 9, 3, 5, 10, ',
+		'Win suggestions'  =>  '6',
+		'Pla suggestions'  =>  '',
 	],
 	'6' => [
 		/**
 		Race 6
 		*/
-		'Runners by odds mvnt'  =>  '2, 4, 1, 8, 3, 7, 9, 5, 6, 10',
+		'win odds mvnt'  =>  '2, 4, 1, 8, 3, 7, 9, 5, 6, 10, ',
+		'pla odds mvnt'  =>  '5, 2, 4, 1, 3, 6, 7, 8, 9, 10, ',
+		'Win suggestions'  =>  '4',
+		'Pla suggestions'  =>  '4',
 	],
 	'7' => [
 		/**
 		Race 7
 		*/
-		'Runners by odds mvnt'  =>  '3, 6, 7, 1, 2, 4, 9, 10, 11, 8, 5',
+		'win odds mvnt'  =>  '3, 6, 7, 1, 2, 4, 9, 10, 11, 8, 5, ',
+		'pla odds mvnt'  =>  '11, 1, 2, 3, 4, 7, 9, 10, 5, 6, 8, ',
+		'Win suggestions'  =>  '6',
+		'Pla suggestions'  =>  '2, 3',
 	],
 	'8' => [
 		/**
 		Race 8
 		*/
-		'Runners by odds mvnt'  =>  '7, 6, 13, 5, 12, 9, 1, 3, 11, 4, 10, 8, 2',
+		'win odds mvnt'  =>  '7, 6, 13, 5, 12, 9, 1, 3, 11, 4, 10, 8, 2, ',
+		'pla odds mvnt'  =>  '6, 7, 13, 9, 1, 3, 12, 4, 5, 11, 8, 2, 10, ',
+		'Win suggestions'  =>  '5, 13',
+		'Pla suggestions'  =>  '9, 6, 13',
 	],
 	'9' => [
 		/**
 		Race 9
 		*/
-		'Runners by odds mvnt'  =>  '9, 13, 1, 4, 6, 10, 11, 2, 7, 3, 5, 8, 12, 14',
+		'win odds mvnt'  =>  '9, 13, 1, 4, 6, 10, 11, 2, 7, 3, 5, 8, 12, 14, ',
+		'pla odds mvnt'  =>  '13, 9, 1, 6, 12, 2, 3, 10, 11, 14, 4, 5, 8, 7, ',
+		'Win suggestions'  =>  '1',
+		'Pla suggestions'  =>  '6, 1',
 	],
 	'10' => [
 		/**
 		Race 10
 		*/
-		'Runners by odds mvnt'  =>  '11, 6, 9, 12, 13, 1, 4, 3, 8, 5, 2, 10, 14, 7',
+		'win odds mvnt'  =>  '11, 6, 9, 12, 13, 1, 4, 3, 8, 5, 2, 10, 14, 7, ',
+		'pla odds mvnt'  =>  '11, 6, 4, 1, 13, 3, 5, 8, 12, 9, 10, 2, 14, 7, ',
+		'Win suggestions'  =>  '11, 12',
+		'Pla suggestions'  =>  '4, 11',
 	],
 ];
